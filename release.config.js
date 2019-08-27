dceef7bfd6655410b0c84c5a7c00de79303c0da3
Louis Pontoise <louis.pontoise@amadeus.com>
2019-08-28 00:56:43 +0900

    ci: setup semantic-release and travisCI

---
+++ release.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer', {
+            'preset': 'angular',
+            // having version increments and builds for these commit types is valuable
+            'releaseRules': [
+                {
+                    'type': 'perf',
+                    'release': 'patch',
+                },
+                {
+                    'type': 'docs',
+                    'release': 'patch',
+                },
+                {
+                    'type': 'style',
+                    'release': 'patch',
+                },
+                {
+                    'type': 'refactor',
+                    'release': 'patch',
+                },
+                {
+                    'type': 'test',
+                    'release': 'patch',
+                },
+                {
+                    'type': 'chore',
+                    'release': 'patch',
+                },
+                {
+                    'type': 'ci',
+                    'release': 'patch',
+                },
+            ],
+        },
+        ],
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        [
+            '@semantic-release/git', {
+            'assets': ['CHANGELOG.md'],
+        },
+        ],
+    ],
+}
