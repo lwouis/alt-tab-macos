58886650c4d03845728bf41c9ee4dcadd9eadd01
Louis Pontoise <louis.pontoise@amadeus.com>
2019-10-23 22:09:59 +0900

    docs: add correct version to app meta-data (closes #47)

--- ci/set_version_in_app.sh
+++ ci/set_version_in_app.sh
@@ -3,4 +3,7 @@
 set -ex
 
 version="$(cat VERSION.txt)"
+# set the version for the app menubar menu text
 sed -i '' -e "s/#VERSION#/$version/" alt-tab-macos/ui/Application.swift
+# set the version in the app meta-data for the AppStore and app "Get Info" panel
+sed -i '' -e "s/#VERSION#/$version/" alt-tab-macos/Info.plist
