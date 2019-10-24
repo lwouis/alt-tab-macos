0a164c731c0837bad81ae07dd49cac3ea12f0b8d
Louis Pontoise <louis.pontoise@amadeus.com>
2019-10-24 23:37:32 +0900

    chore: removed unused image class
    This class was actually providing worse quality, even at high settings, than NSImageView, and for way worse performance

--- alt-tab-macos/ui/HighInterpolationImageView.swift
+++
@@ -1,8 +0,0 @@
-import Cocoa
-
-class HighInterpolationImageView: NSImageView {
-    override func draw(_ dirtyRect: NSRect) {
-        NSGraphicsContext.current!.imageInterpolation = Preferences.thumbnailQuality
-        super.draw(dirtyRect)
-    }
-}
