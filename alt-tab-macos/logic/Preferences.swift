f00131ddd07a8a8827a448bb675eb3afe58b6a05
Louis Pontoise <louis.pontoise@amadeus.com>
2019-10-25 15:13:17 +0900

    refactor: move preference code in the same file

--- alt-tab-macos/logic/Preferences.swift
+++ alt-tab-macos/logic/Preferences.swift
@@ -146,4 +146,26 @@ class Preferences {
     }
 }
 
+struct MacroPreference<T> {
+    let label: String
+    let preferences: T
 
+    init(_ label: String, _ preferences: T) {
+        self.label = label
+        self.preferences = preferences
+    }
+}
+
+class MacroPreferenceHelper<T> {
+    let macros: [MacroPreference<T>]
+    var labels = [String]()
+    var labelToMacro = [String: MacroPreference<T>]()
+
+    init(_ array: [MacroPreference<T>]) {
+        self.macros = array
+        array.forEach {
+            labelToMacro[$0.label] = $0
+            labels.append($0.label)
+        }
+    }
+}
