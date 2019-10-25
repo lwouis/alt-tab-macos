lwouis/alt-tab-macos

commit b6e4826e408847a3c30c2cde7192904012c05c6b

feat: close main window when (meta and) escape is pressed (closes #44)
Also added a KeyCode class to make keycode management nicer

alt-tab-macos/logic/Keyboard.swift
@@ -1,5 +1,14 @@
 import Cocoa
 
+enum KeyCode: UInt16 {
+    case escape = 53
+    case command = 55
+    case capsLock = 57
+    case tab = 58
+    case control = 59
+    case function = 63
+}
+
 class Keyboard {
     static func listenToGlobalEvents(_ delegate: Application) {
         listenToGlobalKeyboardEvents(delegate)
@@ -30,8 +39,10 @@ func keyboardHandler(_ cgEvent: CGEvent, _ delegate: Application) -> Unmanaged<C
     if cgEvent.type == .keyDown || cgEvent.type == .keyUp || cgEvent.type == .flagsChanged {
         if let event = NSEvent(cgEvent: cgEvent) {
             let keyDown = event.type == .keyDown
-            let optionKeyEvent = event.keyCode == Preferences.metaKeyCode
+            let keycode = KeyCode(rawValue: event.keyCode)
+            let optionKeyEvent = keycode == Preferences.metaKeyCode
             let tabKeyEvent = event.keyCode == Preferences.tabKey
+            let escKeyEvent = keycode == KeyCode.escape
             if optionKeyEvent && event.modifiersDown([Preferences.metaModifierFlag!]) {
                 delegate.keyDownMeta()
             } else if tabKeyEvent && event.modifiersDown([Preferences.metaModifierFlag!]) && keyDown {
@@ -40,6 +51,10 @@ func keyboardHandler(_ cgEvent: CGEvent, _ delegate: Application) -> Unmanaged<C
                 return nil
             } else if tabKeyEvent && event.modifiersDown([Preferences.metaModifierFlag!, .shift]) && keyDown {
                 delegate.keyDownMetaShiftTab()
+                // focused app will not receive the event (will not press tab key in that app)
+                return nil
+            } else if escKeyEvent && event.modifiersDown([Preferences.metaModifierFlag!]) && keyDown {
+                delegate.keyDownMetaEsc()
                 // focused app will not receive the event (will not press tab key in that app)
                 return nil
             } else if optionKeyEvent && !keyDown {

alt-tab-macos/logic/Preferences.swift
@@ -7,7 +7,7 @@ let defaults = [
     "maxThumbnailsPerRow": "4",
     "iconSize": "32",
     "fontHeight": "15",
-    "tabKey": "48",
+    "tabKey": String(KeyCode.tab.rawValue),
     "metaKey": Preferences.metaKeyArray[0],
     "windowDisplayDelay": "0",
     "theme": Preferences.themeArray[0]
@@ -31,19 +31,19 @@ class Preferences {
     static var tabKey: UInt16?
     static var highlightBorderColor: NSColor?
     static var highlightBackgroundColor: NSColor?
-    static var metaKeyCode: UInt16?
+    static var metaKeyCode: KeyCode?
     static var metaModifierFlag: NSEvent.ModifierFlags?
     static var windowDisplayDelay: DispatchTimeInterval?
     static var windowCornerRadius: CGFloat?
     static var font: NSFont?
     static var themeArray = [" macOS", "❖ Windows 10"]
     static var metaKeyArray = ["⌥ option", "⌃ control", "⌘ command", "⇪ caps lock", "fn"]
-    static var metaKeyMap: [String: (UInt16, NSEvent.ModifierFlags)] = [
-        metaKeyArray[0]: (58, .option),
-        metaKeyArray[1]: (59, .control),
-        metaKeyArray[2]: (55, .command),
-        metaKeyArray[3]: (57, .capsLock),
-        metaKeyArray[4]: (63, .function),
+    static var metaKeyMap: [String: (KeyCode, NSEvent.ModifierFlags)] = [
+        metaKeyArray[0]: (KeyCode.tab, .option),
+        metaKeyArray[1]: (KeyCode.control, .control),
+        metaKeyArray[2]: (KeyCode.command, .command),
+        metaKeyArray[3]: (KeyCode.capsLock, .capsLock),
+        metaKeyArray[4]: (KeyCode.function, .function),
     ]
 
     private static let defaultsFile = fileFromPreferencesFolder("alt-tab-macos-defaults.json")

alt-tab-macos/ui/Application.swift
@@ -93,13 +93,17 @@ class Application: NSApplication, NSApplicationDelegate, NSWindowDelegate {
         openWindows.count > selectedOpenWindow ? openWindows[selectedOpenWindow] : nil
     }
 
+    func closeThumbnailsPanel() {
+        thumbnailsPanel!.orderOut(nil)
+        appIsBeingUsed = false
+        isFirstSummon = true
+    }
+
     func focusSelectedWindow(_ window: OpenWindow?) {
         workItems.forEach({ $0.cancel() })
         workItems.removeAll()
         window?.focus()
-        thumbnailsPanel!.orderOut(nil)
-        appIsBeingUsed = false
-        isFirstSummon = true
+        closeThumbnailsPanel()
     }
 
     func keyDownMeta() {
@@ -116,6 +120,11 @@ class Application: NSApplication, NSApplicationDelegate, NSWindowDelegate {
     func keyDownMetaShiftTab() {
         debugPrint("meta+shift+tab down")
         selectOtherCell(-1)
+    }
+
+    func keyDownMetaEsc() {
+        debugPrint("meta+esc down")
+        closeThumbnailsPanel()
     }
 
     func keyUpMeta() {

commit e59d51ddab0a760f75635375093ade20c4d9ac75

fix: option key was broken as a meta key
Bug was introduced in b6e4826e4088

alt-tab-macos/logic/Keyboard.swift
@@ -6,6 +6,7 @@ enum KeyCode: UInt16 {
     case escape = 53
     case command = 55
     case capsLock = 57
+    case option = 58
     case control = 59
     case function = 63
     case leftArrow = 123

alt-tab-macos/logic/Preferences.swift
@@ -41,7 +41,7 @@ class Preferences {
         MacroPreference("❖ Windows 10", (2, 0, 0, .white, .clear))
     ])
     static var metaKeyMacro = MacroPreferenceHelper<(KeyCode, NSEvent.ModifierFlags)>([
-        MacroPreference("⌥ option", (.tab, .option)),
+        MacroPreference("⌥ option", (.option, .option)),
         MacroPreference("⌃ control", (.control, .control)),
         MacroPreference("⌘ command", (.command, .command)),
         MacroPreference("⇪ caps lock", (.capsLock, .capsLock)),