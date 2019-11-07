ec6095eb5d2aec441681705fdcd4271c9ca42a72
Louis Pontoise <louis.pontoise@amadeus.com>
2019-11-07 10:55:49 +0900

    refactor: wrap cg and ax apis into classes to clarify codebase

---
+++ alt-tab-macos/logic/AccessibilityApis.swift
@@ -0,0 +1,49 @@
+import Cocoa
+import Foundation
+
+class AccessibilityApis {
+    static func windows(_ cgOwnerPid: pid_t) -> [AXUIElement] {
+        if let windows = attribute(AXUIElementCreateApplication(cgOwnerPid), kAXWindowsAttribute, [AXUIElement].self) {
+            return windows.filter {
+                // workaround: some apps like chrome use a window to implement the search popover
+                let windowBounds = value($0, kAXSizeAttribute, NSSize(), .cgSize)!
+                let isReasonablyBig = windowBounds.width > Preferences.minimumWindowSize && windowBounds.height > Preferences.minimumWindowSize
+                return isReasonablyBig
+            }
+        }
+        return []
+    }
+
+    static func rect(_ element: AXUIElement) -> CGRect {
+        let sizeBefore = value(element, kAXSizeAttribute, NSSize(), .cgSize)!
+        let positionBefore = value(element, kAXPositionAttribute, NSPoint(), .cgPoint)!
+        return CGRect(x: positionBefore.x, y: positionBefore.y, width: sizeBefore.width, height: sizeBefore.height)
+    }
+
+    static func focus(_ element: AXUIElement) {
+        AXUIElementPerformAction(element, kAXRaiseAction as CFString)
+    }
+
+    private static func attribute<T>(_ element: AXUIElement, _ key: String, _ type: T.Type) -> T? {
+        var value: AnyObject?
+        let result = AXUIElementCopyAttributeValue(element, key as CFString, &value)
+        if result == .success, let typedValue = value as? T {
+            return typedValue
+        }
+        return nil
+    }
+
+    static func setAttribute<T>(_ element: AXUIElement, _ value: T, _ attribute: String, _ type: AXValueType) {
+        var v = value
+        AXUIElementSetAttributeValue(element, attribute as CFString, AXValueCreate(type, &v)!)
+    }
+
+    private static func value<T>(_ element: AXUIElement, _ key: String, _ target: T, _ type: AXValueType) -> T? {
+        if let a = attribute(element, key, AXValue.self) {
+            var value = target
+            AXValueGetValue(a, type, &value)
+            return value
+        }
+        return nil
+    }
+}
--- alt-tab-macos/logic/WindowManager.swift
+++ alt-tab-macos/logic/WindowManager.swift
@@ -21,14 +21,14 @@ class OpenWindow {
     }
 
     func computeThumbnail() -> NSImage {
-        let windowImage = CGWindowListCreateImage(.null, .optionIncludingWindow, cgId, [.boundsIgnoreFraming, .bestResolution])
+        let windowImage = CoreGraphicsApis.image(cgId)
         return NSImage(cgImage: windowImage!, size: NSSize(width: windowImage!.width, height: windowImage!.height))
     }
 
     func focus() {
         if let app = NSRunningApplication(processIdentifier: ownerPid!) {
             app.activate(options: [.activateIgnoringOtherApps])
-            AXUIElementPerformAction(target!, kAXRaiseAction as CFString)
+            AccessibilityApis.focus(target!)
         }
     }
 }
@@ -42,46 +42,4 @@ func computeDownscaledSize(_ image: NSImage, _ screen: NSScreen) -> (Int, Int) {
     } else {
         return (Int(thumbnailMaxSize.width), Int(floor(thumbnailMaxSize.width / imageRatio)))
     }
-}
-
-func axWindows(_ cgOwnerPid: pid_t) -> [AXUIElement] {
-    if let windows = AXUIElementCopyAttributeValue(AXUIElementCreateApplication(cgOwnerPid), kAXWindowsAttribute, [AXUIElement].self) {
-        return windows.filter {
-            // workaround: some apps like chrome use a window to implement the search popover
-            let windowBounds = AXValueGetValue($0, kAXSizeAttribute, NSSize(), .cgSize)!
-            let isReasonablyBig = windowBounds.width > Preferences.minimumWindowSize && windowBounds.height > Preferences.minimumWindowSize
-            return isReasonablyBig
-        }
-    }
-    return []
-}
-
-func cgWindows() -> [NSDictionary] {
-    let windows = CGWindowListCopyWindowInfo([.excludeDesktopElements, .optionOnScreenOnly], kCGNullWindowID) as! [NSDictionary]
-    return windows.filter {
-        // workaround: filtering this criteria seems to remove non-windows UI elements
-        let isWindowNotMenubarOrOthers = $0[kCGWindowLayer] as? Int == 0
-        let windowBounds = CGRect(dictionaryRepresentation: $0[kCGWindowBounds] as! NSDictionary)!
-        // workaround: some apps like chrome use a window to implement the search popover
-        let isReasonablyBig = windowBounds.width > Preferences.minimumWindowSize && windowBounds.height > Preferences.minimumWindowSize
-        return isWindowNotMenubarOrOthers && isReasonablyBig
-    }
-}
-
-func AXUIElementCopyAttributeValue<T>(_ element: AXUIElement, _ attribute: String, _ type: T.Type) -> T? {
-    var value: AnyObject?
-    let result = AXUIElementCopyAttributeValue(element, attribute as CFString, &value)
-    if result == .success, let typedValue = value as? T {
-        return typedValue
-    }
-    return nil
-}
-
-func AXValueGetValue<T>(_ element: AXUIElement, _ attribute: String, _ target: T, _ type: AXValueType) -> T? {
-    if let a = AXUIElementCopyAttributeValue(element, attribute, AXValue.self) {
-        var value = target
-        AXValueGetValue(a, type, &value)
-        return value
-    }
-    return nil
 }
