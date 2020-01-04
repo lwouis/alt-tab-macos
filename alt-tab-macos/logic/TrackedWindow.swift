e6e3e8762932a6252b7940f30f7fc99789930ac7
louis.pontoise <>
2020-01-04 22:41:11 +0900

    feat: handle hidden app windows (closes #108)

--- alt-tab-macos/logic/TrackedWindow.swift
+++ alt-tab-macos/logic/TrackedWindow.swift
@@ -3,42 +3,44 @@ import Foundation
 
 class TrackedWindow {
     var cgWindow: CGWindow
-    var ownerPid: pid_t
     var id: CGWindowID
     var title: String
     var thumbnail: NSImage?
     var icon: NSImage?
-    var app: NSRunningApplication?
+    var app: NSRunningApplication
+    var axApp: AXUIElement
     var axWindow: AXUIElement?
+    var isHidden: Bool
     var isMinimized: Bool
     var spaceId: CGSSpaceID?
     var spaceIndex: SpaceIndex?
     var rank: WindowRank?
 
-    init(_ cgWindow: CGWindow, _ cgId: CGWindowID, _ ownerPid: pid_t, _ isMinimized: Bool, _ axWindow: AXUIElement?, _ spaceId: CGSSpaceID?, _ spaceIndex: SpaceIndex?, _ rank: WindowRank?) {
+    init(_ cgWindow: CGWindow, _ cgId: CGWindowID, _ app: NSRunningApplication, _ axApp: AXUIElement, _ isHidden: Bool, _ isMinimized: Bool, _ axWindow: AXUIElement?, _ spaceId: CGSSpaceID?, _ spaceIndex: SpaceIndex?, _ rank: WindowRank?) {
         self.cgWindow = cgWindow
         self.id = cgId
-        self.ownerPid = ownerPid
         let cgTitle = cgWindow.value(.name, String.self)
         let cgOwnerName = cgWindow.value(.ownerName, String.self)
         // for some reason Google Chrome uses a unicode 0-width no-break space character in their empty window title
         self.title = cgTitle != nil && cgTitle != "" && cgTitle != "﻿" ? cgTitle! : cgOwnerName ?? ""
-        self.app = NSRunningApplication(processIdentifier: ownerPid)
-        self.icon = self.app?.icon
+        self.app = app
+        self.axApp = axApp
+        self.icon = self.app.icon
         if let cgImage = cgId.screenshot() {
             self.thumbnail = NSImage(cgImage: cgImage, size: NSSize(width: cgImage.width, height: cgImage.height))
         }
         self.axWindow = axWindow
+        self.isHidden = isHidden
         self.isMinimized = isMinimized
         self.spaceId = spaceId
-        // System Preferences windows appear on all spaces, so we make them the current space
-        self.spaceIndex = app?.bundleIdentifier == "com.apple.systempreferences" ? Spaces.currentSpaceIndex : spaceIndex
+        self.spaceIndex = spaceIndex
         self.rank = rank
     }
 
     func focus() {
-        if axWindow == nil {
-            axWindow = id.AXUIElementOfOtherSpaceWindow(ownerPid)
+        let onCurrentSpace = axWindow != nil
+        if !onCurrentSpace {
+            axWindow = id.AXUIElementOfOtherSpaceWindow(axApp)
         }
         axWindow?.focus(id)
     }
