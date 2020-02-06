fd3e7680f4b8f76da011f4a10d8b0876f6b8b958
louis.pontoise <unknown>
2020-02-07 00:15:06 +0900

    feat: cleaner layout and explanation text

--- alt-tab-macos/ui/generic-components/text/TextArea.swift
+++ alt-tab-macos/ui/generic-components/text/TextArea.swift
@@ -1,15 +1,22 @@
 import Cocoa
 
 class TextArea: NSTextView {
-    static let paddingX = CGFloat(5)
-    static let paddingY = CGFloat(10)
+    static let padding = CGFloat(10)
+    static let magicOffset = CGFloat(3)
     @objc var placeholderAttributedString: NSAttributedString?
 
     convenience init(_ width: CGFloat, _ height: CGFloat, _ placeholder: String) {
         self.init(frame: .zero)
         font = NSFont.systemFont(ofSize: NSFont.systemFontSize)
-        textContainerInset = NSSize(width: TextArea.paddingX, height: TextArea.paddingY)
-        fit(font!.xHeight * width + TextArea.paddingX * 2, NSFont.systemFontSize * height + TextArea.paddingY * 2)
-        placeholderAttributedString = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor: NSColor.gray])
+        textContainerInset = NSSize(width: TextArea.padding, height: TextArea.padding)
+        textContainer!.lineFragmentPadding = 0
+        let paragraphStyle = NSParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+        paragraphStyle.maximumLineHeight = NSFont.systemFontSize + TextArea.magicOffset
+        placeholderAttributedString = NSAttributedString(string: placeholder, attributes: [
+            NSAttributedString.Key.font : NSFont.systemFont(ofSize: NSFont.systemFontSize),
+            NSAttributedString.Key.foregroundColor: NSColor.gray,
+            NSAttributedString.Key.paragraphStyle: paragraphStyle,
+        ])
+        fit(font!.xHeight * width + TextArea.padding * 2, NSFont.systemFontSize * height + TextArea.padding * 2 + TextArea.magicOffset)
     }
 }
