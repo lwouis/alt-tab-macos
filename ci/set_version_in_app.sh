9393bc26ffcedcc112aef81fc8634c421e0060f1
louis.pontoise <>
2020-02-07 18:14:51 +0900

    feat: add licence to about page
    Also use data from Info.plist to make it more DRY

--- ci/set_version_in_app.sh
+++ ci/set_version_in_app.sh
@@ -3,7 +3,5 @@
 set -exu
 
 version="$(cat VERSION.txt)"
-# set the version displayed in the app UI
-sed -i '' -e "s/#VERSION#/$version/" alt-tab-macos/ui/App.swift
 # set the version in the app meta-data for the AppStore and app "Get Info" panel
 sed -i '' -e "s/#VERSION#/$version/" alt-tab-macos/Info.plist
