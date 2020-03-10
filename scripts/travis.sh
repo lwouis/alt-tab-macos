33bb2ce98f1ab9bff8961f0c5e970b23f40fecc4
Louis Pontoise <lwouis@gmail.com>
2020-03-10 15:20:55 +0900

    ci: travis stop-on-error was broken
    see https://github.com/travis-ci/travis-ci/issues/1066

---
+++ scripts/travis.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -exu
+
+env | sort
+npm ci
+npx commitlint-travis
+if [ $IS_RELEASE ]; then
+  scripts/determine_version_and_changelog.sh
+  scripts/replace_environment_variables_in_app.sh
+fi
+pod install
+scripts/import_codesign_certificate_into_keychain.sh
+xcodebuild -workspace alt-tab-macos.xcworkspace -scheme Release -derivedDataPath $XCODE_BUILD_PATH
+if [ $IS_RELEASE ]; then
+  scripts/package_and_notarize_release.sh
+  scripts/update_appcast.sh
+  scripts/update_homebrew_cask.sh
+  npx semantic-release
+fi
