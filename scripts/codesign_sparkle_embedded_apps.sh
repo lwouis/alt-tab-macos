203dbfc69b954630810bd7cf3f4ce166a53a75c5
Louis Pontoise <lwouis@gmail.com>
2020-03-10 16:13:47 +0900

    ci: codesign inside main .app, not in the pods folder

--- scripts/codesign_sparkle_embedded_apps.sh
+++ scripts/codesign_sparkle_embedded_apps.sh
@@ -5,4 +5,4 @@ set -exu
 # codesign --deep is only 1 level deep. It misses Sparkle embedded app AutoUpdate
 # this build phase script works around the issue
 
-codesign --verbose --force --sign "$CODE_SIGN_IDENTITY" $OTHER_CODE_SIGN_FLAGS "${PODS_ROOT}/Sparkle/Sparkle.framework/Resources/Autoupdate.app"
+codesign --verbose --force --sign "$CODE_SIGN_IDENTITY" $OTHER_CODE_SIGN_FLAGS "${CODESIGNING_FOLDER_PATH}/Contents/Frameworks/Sparkle.framework/Versions/A/Resources/Autoupdate.app"
