502e297e0792b03e3b528f8511cffb26f419c37a
Louis Pontoise <lwouis@gmail.com>
2020-03-10 20:48:12 +0900

    ci: fix cask-repair ssh access issue 3

--- scripts/update_homebrew_cask.sh
+++ scripts/update_homebrew_cask.sh
@@ -5,8 +5,8 @@ set -exu
 version="$(cat $VERSION_FILE)"
 
 # add github ssh key
-echo "$GITHUB_SSH_KEY" > github_ssh
+echo "$GITHUB_SSH_KEY" | base64 --decode > github_ssh
 chmod 600 github_ssh
-ssh-add github_ssh
+ssh-add -K github_ssh
 
 cask-repair --blind-submit --cask-version "$version" alt-tab
