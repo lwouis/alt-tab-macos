7ca8c52629efde694647edcded333f34ced8ac2e
Louis Pontoise <lwouis@gmail.com>
2020-04-25 11:45:07 +0900

    refactor: add todo and fix indentation

--- src/ui/Menubar.swift
+++ src/ui/Menubar.swift
@@ -9,27 +9,26 @@ class Menubar {
         item.button!.imageScaling = .scaleProportionallyUpOrDown
         item.menu = NSMenu()
         item.menu!.addItem(
-                withTitle: NSLocalizedString("Show", comment: "Menubar option"),
-                action: #selector(App.app.showUi),
-                keyEquivalent: ""
-        )
+            withTitle: NSLocalizedString("Show", comment: "Menubar option"),
+            action: #selector(App.app.showUi),
+            keyEquivalent: "")
         item.menu!.addItem(
-                withTitle: NSLocalizedString("Preferences…", comment: "Menubar option"),
-                action: #selector(App.app.showPreferencesPanel),
-                keyEquivalent: ",")
+            withTitle: NSLocalizedString("Preferences…", comment: "Menubar option"),
+            action: #selector(App.app.showPreferencesPanel),
+            keyEquivalent: ",")
         item.menu!.addItem(
-                withTitle: NSLocalizedString("Check for updates…", comment: "Menubar option"),
-                action: #selector(App.app.checkForUpdatesNow),
-                keyEquivalent: "")
+            withTitle: NSLocalizedString("Check for updates…", comment: "Menubar option"),
+            action: #selector(App.app.checkForUpdatesNow),
+            keyEquivalent: "")
         item.menu!.addItem(
-                withTitle: NSLocalizedString("Send feedback…", comment: "Menubar option"),
-                action: #selector(App.app.showFeedbackPanel),
-                keyEquivalent: "")
+            withTitle: NSLocalizedString("Send feedback…", comment: "Menubar option"),
+            action: #selector(App.app.showFeedbackPanel),
+            keyEquivalent: "")
         item.menu!.addItem(NSMenuItem.separator())
         item.menu!.addItem(
             withTitle: String(format: NSLocalizedString("Quit %@", comment: "Menubar option. %@ is AltTab"), App.name),
-                action: #selector(NSApplication.terminate(_:)),
-                keyEquivalent: "q")
+            action: #selector(NSApplication.terminate(_:)),
+            keyEquivalent: "q")
         return item
     }
 }
