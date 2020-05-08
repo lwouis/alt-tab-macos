2df72af8091f984c1a92725258dacef0abf5b0e3
Louis Pontoise <lwouis@gmail.com>
2020-05-08 22:58:15 +0900

    docs: move changelog to docs folder

--- release.config.js
+++ release.config.js
@@ -1,51 +1,28 @@
 module.exports = {
     plugins: [
-        [
-            '@semantic-release/commit-analyzer', {
+        ['@semantic-release/commit-analyzer', {
             'preset': 'angular',
             // having version increments and builds for these commit types is valuable
             'releaseRules': [
-                {
-                    'type': 'perf',
-                    'release': 'patch',
-                },
-                {
-                    'type': 'docs',
-                    'release': 'patch',
-                },
-                {
-                    'type': 'style',
-                    'release': 'patch',
-                },
-                {
-                    'type': 'refactor',
-                    'release': 'patch',
-                },
-                {
-                    'type': 'test',
-                    'release': 'patch',
-                },
-                {
-                    'type': 'chore',
-                    'release': 'patch',
-                },
-                {
-                    'type': 'ci',
-                    'release': 'patch',
-                },
+                {'type': 'perf', 'release': 'patch'},
+                {'type': 'docs', 'release': 'patch'},
+                {'type': 'style', 'release': 'patch'},
+                {'type': 'refactor', 'release': 'patch'},
+                {'type': 'test', 'release': 'patch'},
+                {'type': 'chore', 'release': 'patch'},
+                {'type': 'ci', 'release': 'patch'},
             ],
-        },
-        ],
+        }],
         '@semantic-release/release-notes-generator',
-        '@semantic-release/changelog',
-        [
-            '@semantic-release/git', {
+        ['@semantic-release/changelog', {
+            'changelogFile': 'docs/CHANGELOG.md',
+        }],
+        ['@semantic-release/git', {
             'assets': [
                 'docs/CONTRIBUTORS.md',
-                'CHANGELOG.md',
+                'docs/CHANGELOG.md',
                 'appcast.xml'
             ],
-        },
-        ],
+        }],
     ],
 }
