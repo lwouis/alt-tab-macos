408b800a23362206fc250df50ffe95ae4b2a84bb
Louis Pontoise <lwouis@gmail.com>
2020-05-21 18:14:34 +0900

    fix: don't freeze when sending a command to an frozen window

--- src/logic/DispatchQueues.swift
+++ src/logic/DispatchQueues.swift
@@ -2,6 +2,6 @@ import Foundation
 
 // OS events should be observed on background threads
 class DispatchQueues {
-    static let accessibilityCommands = DispatchQueue(label: "accessibilityCommands", qos: .userInteractive)
+    static let accessibilityCommands = DispatchQueue(label: "accessibilityCommands", qos: .userInteractive, attributes: .concurrent)
     static let keyboardEvents = DispatchQueue(label: "keyboardEvents", qos: .userInteractive)
 }
