76b841cbd2ef9aa05a9def6b23443a9d0a71be0d
Louis Pontoise <lwouis@gmail.com>
2020-07-11 03:21:02 +0900

    ci: disable pod install as it is undeterministic

--- scripts/ensure_generated_files_are_up_to_date.sh
+++ scripts/ensure_generated_files_are_up_to_date.sh
@@ -4,7 +4,7 @@ brew install jq
 scripts/update_contributors.sh
 scripts/import_l10n_strings_from_poeditor.sh
 scripts/extract_l10n_strings.sh
-pod install
+# pod install
 
 git status
 git --no-pager diff
