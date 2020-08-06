81a3553af2eb2daef22dcb90242d7baf1134f6fd
Louis Pontoise <lwouis@gmail.com>
2020-08-07 00:46:38 +0900

    perf: reduce size of app icon (closes #169)

--- scripts/convert_iconset_to_icns.sh
+++ scripts/convert_iconset_to_icns.sh
@@ -2,4 +2,7 @@
 
 set -exu
 
-iconutil --convert icns resources/icons/app-icon.iconset
+# xcode or iconutil re-encode PNGs, increasing the final .icns file for no reason; we use a third-party tool instead
+# see https://github.com/lwouis/createicns
+scripts/createicns resources/icons/app-icon.iconset
+mv app-icon.icns resources/icons/app-icon.icns
