522633b2eb1a2c1973347791995ddf6d969b4aaa
Louis Pontoise <lwouis@gmail.com>
2020-08-26 02:13:41 +0900

    fix: second blacklist was too tall on some systems

--- src/ui/preferences-window/tabs/BlacklistsTab.swift
+++ src/ui/preferences-window/tabs/BlacklistsTab.swift
@@ -4,10 +4,10 @@ import Sparkle
 class BlacklistsTab {
     static func initTab() -> NSView {
         let dontShowBlacklistLabel = NSTextField(labelWithString: NSLocalizedString("Don’t show windows from these apps", comment: ""))
-        let dontShowBlacklist = LabelAndControl.makeTextArea(50, 3, Preferences.dontShowBlacklist.joined(separator: "\n"), "dontShowBlacklist")
+        let dontShowBlacklist = LabelAndControl.makeTextArea(50, 6, Preferences.dontShowBlacklist.joined(separator: "\n"), "dontShowBlacklist")
         let disableShortcutsBlacklistLabel = NSTextField(labelWithString: NSLocalizedString("Ignore shortcuts while a window from these apps is active", comment: ""))
         let disableShortcutsBlacklistCheckbox = LabelAndControl.makeLabelWithCheckbox(NSLocalizedString("Only if the window is fullscreen", comment: ""), "disableShortcutsBlacklistOnlyFullscreen", labelPosition: .right)
-        let disableShortcutsBlacklist = LabelAndControl.makeTextArea(50, 3, Preferences.disableShortcutsBlacklist.joined(separator: "\n"), "disableShortcutsBlacklist")
+        let disableShortcutsBlacklist = LabelAndControl.makeTextArea(50, 6, Preferences.disableShortcutsBlacklist.joined(separator: "\n"), "disableShortcutsBlacklist")
 
         let grid = GridView([
             [dontShowBlacklistLabel],
