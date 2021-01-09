a10fabd5974cc1cb30c31f357f6a0d9bce73ff70
Ben Lu <ayroblu@gmail.com>
2021-01-09 22:26:38 +0000

    fix: format server

--- src/logic/Server.swift
+++ src/logic/Server.swift
@@ -5,10 +5,10 @@ let server = HttpServer()
 func startServer() {
     setEndpoints()
     do {
-      try server.start(9999, forceIPv4: true)
-      print("Server has started (port: \(try server.port()))")
+        try server.start(9999, forceIPv4: true)
+        print("Server has started (port: \(try server.port()))")
     } catch {
-      print("Server start error: \(error)")
+        print("Server start error: \(error)")
     }
 }
 
@@ -23,23 +23,33 @@ private func setEndpoints() {
 }
 
 func getWindows() -> HttpResponse {
-    let windowData = Windows.list.map({["name": $0.title ?? "(Unknown)", "isFullscreen": $0.isFullscreen, "isMinimized": $0.isMinimized, "spaceIndex": $0.spaceIndex, "lastFocusOrder": $0.lastFocusOrder, "application": $0.application.runningApplication.localizedName ?? "(Unknown)", "applicationBundleUrl": $0.application.runningApplication.bundleURL?.absoluteString ?? "file:///", "windowId": $0.cgWindowId, "isHidden": $0.isHidden, "isWindowlessApp": $0.isWindowlessApp]})
-    
-    let payload: [String : Any] = ["windows": windowData, "version": 1]
+    let windowData = Windows.list.map({
+        [
+            "name": $0.title ?? "(Unknown)", "isFullscreen": $0.isFullscreen,
+            "isMinimized": $0.isMinimized, "spaceIndex": $0.spaceIndex,
+            "lastFocusOrder": $0.lastFocusOrder,
+            "application": $0.application.runningApplication.localizedName ?? "(Unknown)",
+            "applicationBundleUrl": $0.application.runningApplication.bundleURL?.absoluteString
+                ?? "file:///", "windowId": $0.cgWindowId, "isHidden": $0.isHidden,
+            "isWindowlessApp": $0.isWindowlessApp,
+        ]
+    })
+
+    let payload: [String: Any] = ["windows": windowData, "version": 1]
 
     return HttpResponse.ok(.json(payload))
 }
 
 func closeWindow(_ request: HttpRequest) -> HttpResponse {
     let form = request.parseUrlencodedForm()
-    
+
     return form.first(where: { $0.0 == "windowId" })
-        .flatMap({Int($0.1)})
-        .flatMap({windowId in
-            Windows.list.first(where: {$0.cgWindowId == windowId})
-        }).map({window in
+        .flatMap({ Int($0.1) })
+        .flatMap({ windowId in
+            Windows.list.first(where: { $0.cgWindowId == windowId })
+        }).map({ window in
             window.close()
-        }).map({_ in
+        }).map({ _ in
             HttpResponse.ok(.text(""))
         }) ?? HttpResponse.badRequest(.text(""))
 }
