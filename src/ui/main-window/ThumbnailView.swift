5f5df82560366791e62568d7ed97efa6cdafb2a4
Jeff Bargmann <2670022+jeffbargmann@users.noreply.github.com>
2021-10-17 10:19:29 -0700

    Fix responding to mouse move during drag-drop
    App continued to respond to mouse moves during drag-drop even if "respond to mouse" setting was disabled.

--- src/ui/main-window/ThumbnailView.swift
+++ src/ui/main-window/ThumbnailView.swift
@@ -213,7 +213,9 @@ class ThumbnailView: NSStackView {
     }
 
     override func draggingUpdated(_ sender: NSDraggingInfo) -> NSDragOperation {
-        mouseMovedCallback()
+        if Preferences.mouseHoverEnabled && !isHighlighted {
+            mouseMovedCallback()
+        }
         return .link
     }
 
