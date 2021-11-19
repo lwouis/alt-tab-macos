6352964a49c0229a0daa89530142adb80e555021
Louis Pontoise <lwouis@gmail.com>
2021-11-19 22:09:51 +0900

    ci: make release build on pull-requests as well

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -12,16 +12,16 @@ scripts/ensure_generated_files_are_up_to_date.sh
 if [ $IS_RELEASE ]; then
   scripts/determine_version.sh
   scripts/replace_environment_variables_in_app.sh
-  scripts/codesign/setup_ci_master.sh
-  xcodebuild -workspace alt-tab-macos.xcworkspace -scheme Release -derivedDataPath DerivedData
-  file "$TRAVIS_BUILD_DIR/$XCODE_BUILD_PATH/$APP_NAME.app/Contents/MacOS/$APP_NAME"
+fi
+
+scripts/codesign/setup_ci_master.sh
+xcodebuild -workspace alt-tab-macos.xcworkspace -scheme Release -derivedDataPath DerivedData
+file "$TRAVIS_BUILD_DIR/$XCODE_BUILD_PATH/$APP_NAME.app/Contents/MacOS/$APP_NAME"
+
+if [ $IS_RELEASE ]; then
   scripts/package_and_notarize_release.sh
   scripts/upload_symbols_to_appcenter.sh
   scripts/update_appcast.sh
   npx semantic-release
   scripts/update_website.sh
-else
-  scripts/codesign/setup_ci_pr.sh
-  xcodebuild -workspace alt-tab-macos.xcworkspace -scheme Debug -derivedDataPath DerivedData
-  file "$TRAVIS_BUILD_DIR/$XCODE_BUILD_PATH/$APP_NAME.app/Contents/MacOS/$APP_NAME"
 fi
