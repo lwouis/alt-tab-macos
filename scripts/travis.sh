f6f7ccf49c583fe3f6c9070fae5316fcf5c5d13e
Martin Wilhelmi <mnin@mnin.de>
2021-11-19 21:35:47 +0900

    ci: show architectures inside binary after build

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -14,6 +14,7 @@ if [ $IS_RELEASE ]; then
   scripts/replace_environment_variables_in_app.sh
   scripts/codesign/setup_ci_master.sh
   xcodebuild -workspace alt-tab-macos.xcworkspace -scheme Release -derivedDataPath DerivedData
+  file "$TRAVIS_BUILD_DIR/$XCODE_BUILD_PATH/$APP_NAME.app/Contents/MacOS/$APP_NAME"
   scripts/package_and_notarize_release.sh
   scripts/upload_symbols_to_appcenter.sh
   scripts/update_appcast.sh
@@ -22,4 +23,5 @@ if [ $IS_RELEASE ]; then
 else
   scripts/codesign/setup_ci_pr.sh
   xcodebuild -workspace alt-tab-macos.xcworkspace -scheme Debug -derivedDataPath DerivedData
+  file "$TRAVIS_BUILD_DIR/$XCODE_BUILD_PATH/$APP_NAME.app/Contents/MacOS/$APP_NAME"
 fi
