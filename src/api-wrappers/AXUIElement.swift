82555fbb19c9155f4640ffa57c8828de21297be1
Zachary Wander <zachary.wander@gmail.com>
2022-08-14 19:17:59 -0400

    chore: reorganize how window exceptions are checked

--- src/api-wrappers/AXUIElement.swift
+++ src/api-wrappers/AXUIElement.swift
@@ -7,6 +7,7 @@ import ApplicationServices.HIServices.AXAttributeConstants
 import ApplicationServices.HIServices.AXActionConstants
 
 // if the window server is busy, it may not reply to AX calls. We retry right before the call times-out and returns a bogus value
+
 func retryAxCallUntilTimeout(_ group: DispatchGroup? = nil, _ timeoutInSeconds: Double = Double(AXUIElement.globalTimeoutInSeconds), _ fn: @escaping () throws -> Void, _ startTime: DispatchTime = DispatchTime.now()) {
     group?.enter()
     BackgroundWork.axCallsQueue.async {
@@ -34,7 +35,10 @@ extension AXUIElement {
     // See https://humanbenchmark.com/tests/reactiontime
     static let retryDelayInMilliseconds = 250
 
+    static let minWindowSize = 100.0
+
     // default timeout for AX calls is 6s. We increase it in order to avoid retrying every 6s, thus saving resources
+
     static func setGlobalTimeout() {
         // we add 5s to make sure to not do an extra retry
         AXUIElementSetMessagingTimeout(AXUIElementCreateSystemWide(), globalTimeoutInSeconds + 5)
@@ -43,9 +47,9 @@ extension AXUIElement {
     func axCallWhichCanThrow<T>(_ result: AXError, _ successValue: inout T) throws -> T? {
         switch result {
             case .success: return successValue
-            // .cannotComplete can happen if the app is unresponsive; we throw in that case to retry until the call succeeds
+                // .cannotComplete can happen if the app is unresponsive; we throw in that case to retry until the call succeeds
             case .cannotComplete: throw AxError.runtimeError
-            // for other errors it's pointless to retry
+                // for other errors it's pointless to retry
             default: return nil
         }
     }
@@ -81,99 +85,16 @@ extension AXUIElement {
         // Activity Monitor main window subrole is "AXDialog" for a brief moment at launch; it then becomes "AXStandardWindow"
 
         // Some non-windows have cgWindowId == 0 (e.g. windows of apps starting at login with the checkbox "Hidden" checked)
+
+        let opts = WindowCheckOptions(runningApp: runningApp, title: title, subrole: subrole, wid: wid, level: level, role: role, size: size)
+
         return wid != 0 &&
-            size != nil && size!.width > 100 && size!.height > 100 &&
-            (books(runningApp) || keynote(runningApp) || iina(runningApp) || (
-                // CGWindowLevel == .normalWindow helps filter out iStats Pro and other top-level pop-overs, and floating windows
+                size != nil && size!.width > minWindowSize && size!.height > minWindowSize &&
                 level == CGWindow.normalLevel &&
-                    jetbrainApp(runningApp, title, subrole) &&
-                    ([kAXStandardWindowSubrole, kAXDialogSubrole].contains(subrole) ||
-                        openBoard(runningApp) ||
-                        adobeAudition(runningApp, subrole) ||
-                        steam(runningApp, title, role) ||
-                        worldOfWarcraft(runningApp, role) ||
-                        battleNetBootstrapper(runningApp, role) ||
-                        firefoxFullscreenVideo(runningApp, role) ||
-                        vlcFullscreenVideo(runningApp, role) ||
-                        androidEmulator(runningApp, title) ||
-                        sanGuoShaAirWD(runningApp) ||
-                        dvdFab(runningApp) ||
-                        drBetotte(runningApp))))
-    }
-
-    private static func jetbrainApp(_ runningApp: NSRunningApplication, _ title: String?, _ subrole: String?) -> Bool {
-        // jetbrain apps sometimes generate non-windows that pass all checks in isActualWindow
-        // they have no title, so we can filter them out based on that
-        return runningApp.bundleIdentifier?.range(of: "^com\\.(jetbrains\\.|google\\.android\\.studio).*?$", options: .regularExpression) == nil ||
-            (subrole == kAXStandardWindowSubrole || title != nil && title != "")
-    }
-
-    private static func iina(_ runningApp: NSRunningApplication) -> Bool {
-        // IINA.app can have videos float (level == 2 instead of 0)
-        // there is also complex animations during which we may or may not consider the window not a window
-        return runningApp.bundleIdentifier == "com.colliderli.iina"
-    }
-
-    private static func keynote(_ runningApp: NSRunningApplication) -> Bool {
-        // apple Keynote has a fake fullscreen window when in presentation mode
-        // it covers the screen with a AXUnknown window instead of using standard fullscreen mode
-        return runningApp.bundleIdentifier == "com.apple.iWork.Keynote"
-    }
-
-    private static func openBoard(_ runningApp: NSRunningApplication) -> Bool {
-        // OpenBoard is a ported app which doesn't use standard macOS windows
-        return runningApp.bundleIdentifier == "org.oe-f.OpenBoard"
-    }
-
-    private static func adobeAudition(_ runningApp: NSRunningApplication, _ subrole: String?) -> Bool {
-        return runningApp.bundleIdentifier == "com.adobe.Audition" && subrole == kAXFloatingWindowSubrole
-    }
-
-    private static func books(_ runningApp: NSRunningApplication) -> Bool {
-        // Books.app has animations on window creation. This means windows are originally created with subrole == AXUnknown or isOnNormalLevel == false
-        return runningApp.bundleIdentifier == "com.apple.iBooksX"
-    }
-
-    private static func worldOfWarcraft(_ runningApp: NSRunningApplication, _ role: String?) -> Bool {
-        return runningApp.bundleIdentifier == "com.blizzard.worldofwarcraft" && role == kAXWindowRole
-    }
-
-    private static func battleNetBootstrapper(_ runningApp: NSRunningApplication, _ role: String?) -> Bool {
-        // Battlenet bootstrapper windows have subrole == AXUnknown
-        return runningApp.bundleIdentifier == "net.battle.bootstrapper" && role == kAXWindowRole
-    }
-
-    private static func drBetotte(_ runningApp: NSRunningApplication) -> Bool {
-        return runningApp.bundleIdentifier == "com.ssworks.drbetotte"
-    }
-
-    private static func dvdFab(_ runningApp: NSRunningApplication) -> Bool {
-        return runningApp.bundleIdentifier == "com.goland.dvdfab.macos"
-    }
-
-    private static func sanGuoShaAirWD(_ runningApp: NSRunningApplication) -> Bool {
-        return runningApp.bundleIdentifier == "SanGuoShaAirWD"
-    }
-
-    private static func steam(_ runningApp: NSRunningApplication, _ title: String?, _ role: String?) -> Bool {
-        // All Steam windows have subrole == AXUnknown
-        // some dropdown menus are not desirable; they have title == "", or sometimes role == nil when switching between menus quickly
-        return runningApp.bundleIdentifier == "com.valvesoftware.steam" && title != "" && role != nil
-    }
-
-    private static func firefoxFullscreenVideo(_ runningApp: NSRunningApplication, _ role: String?) -> Bool {
-        // Firefox fullscreen video have subrole == AXUnknown if fullscreen'ed when the base window is not fullscreen
-        return (runningApp.bundleIdentifier?.hasPrefix("org.mozilla.firefox") ?? false) && role == kAXWindowRole
-    }
-
-    private static func vlcFullscreenVideo(_ runningApp: NSRunningApplication, _ role: String?) -> Bool {
-        // VLC fullscreen video have subrole == AXUnknown if fullscreen'ed
-        return (runningApp.bundleIdentifier?.hasPrefix("org.videolan.vlc") ?? false) && role == kAXWindowRole
-    }
-
-    private static func androidEmulator(_ runningApp: NSRunningApplication, _ title: String?) -> Bool {
-        // android emulator small vertical menu is a "window" with empty title; we exclude it
-        return title != "" && Applications.isAndroidEmulator(runningApp)
+                ([kAXStandardWindowSubrole, kAXDialogSubrole].contains(subrole) ||
+                        windowChecks.contains(where: { element in
+                            element.check(opts)
+                        }))
     }
 
     func position() throws -> CGPoint? {
---
+++ src/logic/windowcheck/WindowCheck.swift
@@ -0,0 +1,189 @@
+//
+// Created by Zachary Wander on 8/14/22.
+// Copyright (c) 2022 lwouis. All rights reserved.
+//
+
+/**
+ The collection of all window checkers.
+ If a new check is created, its instance should be added here.
+ */
+let windowChecks: [WindowCheck] = [
+    JetBrainsWindowCheck(),
+    IINAWindowCheck(),
+    KeyNoteWindowCheck(),
+    OpenBoardWindowCheck(),
+    AdobeAuditionWindowCheck(),
+    BooksWindowCheck(),
+    WorldOfWarcraftWindowCheck(),
+    BattleNetBootstrapperWindowCheck(),
+    DrBetotteWindowCheck(),
+    DVDFabWindowCheck(),
+    SanGuoShaAirWDWindowCheck(),
+    SteamWindowCheck(),
+    FirefoxFullscreenVideoWindowCheck(),
+    VLCFullscreenVideoWindowCheck(),
+    AndroidEmulatorWindowCheck()
+]
+
+struct WindowCheckOptions {
+    var runningApp: NSRunningApplication
+    var title: String?
+    var subrole: String?
+    var wid: CGWindowID
+    var level: CGWindowLevel
+    var role: String?
+    var size: CGSize?
+}
+
+/**
+ The base class for performing a window exception check.
+ Subclasses should override [isValidWindow] and perform checks based
+ on the passed [WindowCheckOptions], returning true if the window should
+ show in the switcher.
+ */
+class WindowCheck {
+    func check(_ opts: WindowCheckOptions) -> Bool {
+        return isValidWindow(opts) && isValidSize(opts.size) && isValidLevel(opts.level)
+    }
+
+    /**
+     This allows for overriding the default requirements of windows being at least 100x100
+     to show in the switcher.
+     - Parameter size: the size of the window being checked.
+     - Returns: true if the window's size is correct.
+     */
+    func isValidSize(_ size: CGSize?) -> Bool {
+        return size != nil && size!.width > AXUIElement.minWindowSize && size!.height > AXUIElement.minWindowSize
+    }
+
+    /**
+     Most windows should only show in the switcher if they have a normal level.
+     Some apps (Books, KeyNote, IINA) should show up no matter their level.
+     - Parameter level: the current window level.
+     - Returns: true if the window's level is correct.
+     */
+    func isValidLevel(_ level: CGWindowLevel) -> Bool {
+        return level == CGWindow.normalLevel
+    }
+
+    func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        fatalError("Must override!")
+    }
+}
+
+class JetBrainsWindowCheck : WindowCheck {
+    override func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        // jetbrain apps sometimes generate non-windows that pass all checks in isActualWindow
+        // they have no title, so we can filter them out based on that
+        return opts.runningApp.bundleIdentifier?.range(of: "^com\\.(jetbrains\\.|google\\.android\\.studio).*?$", options: .regularExpression) != nil &&
+                (opts.subrole == kAXStandardWindowSubrole || opts.title != nil && opts.title != "")
+    }
+}
+
+class IINAWindowCheck : WindowCheck {
+    override func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        // IINA.app can have videos float (level == 2 instead of 0)
+        // there is also complex animations during which we may or may not consider the window not a window
+        return opts.runningApp.bundleIdentifier == "com.colliderli.iina"
+    }
+
+    override func isValidLevel(_ level: CGWindowLevel) -> Bool {
+        return true
+    }
+}
+
+class KeyNoteWindowCheck : WindowCheck {
+    override func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        // apple Keynote has a fake fullscreen window when in presentation mode
+        // it covers the screen with a AXUnknown window instead of using standard fullscreen mode
+        return opts.runningApp.bundleIdentifier == "com.apple.iWork.Keynote"
+    }
+
+    override func isValidLevel(_ level: CGWindowLevel) -> Bool {
+        return true
+    }
+}
+
+class OpenBoardWindowCheck : WindowCheck {
+    override func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        // OpenBoard is a ported app which doesn't use standard macOS windows
+        return opts.runningApp.bundleIdentifier == "org.oe-f.OpenBoard"
+    }
+}
+
+class AdobeAuditionWindowCheck : WindowCheck {
+    override func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        return opts.runningApp.bundleIdentifier == "com.adobe.Audition" && opts.subrole == kAXFloatingWindowSubrole
+    }
+}
+
+class BooksWindowCheck : WindowCheck {
+    override func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        // Books.app has animations on window creation. This means windows are originally created with subrole == AXUnknown or isOnNormalLevel == false
+        return opts.runningApp.bundleIdentifier == "com.apple.iBooksX"
+    }
+
+    override func isValidLevel(_ level: CGWindowLevel) -> Bool {
+        return true
+    }
+}
+
+class WorldOfWarcraftWindowCheck : WindowCheck {
+    override func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        return opts.runningApp.bundleIdentifier == "com.blizzard.worldofwarcraft" && opts.role == kAXWindowRole
+    }
+}
+
+class BattleNetBootstrapperWindowCheck : WindowCheck {
+    override func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        // Battlenet bootstrapper windows have subrole == AXUnknown
+        return opts.runningApp.bundleIdentifier == "net.battle.bootstrapper" && opts.role == kAXWindowRole
+    }
+}
+
+class DrBetotteWindowCheck : WindowCheck {
+    override func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        return opts.runningApp.bundleIdentifier == "com.ssworks.drbetotte"
+    }
+}
+
+class DVDFabWindowCheck : WindowCheck {
+    override func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        return opts.runningApp.bundleIdentifier == "com.goland.dvdfab.macos"
+    }
+}
+
+class SanGuoShaAirWDWindowCheck : WindowCheck {
+    override func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        return opts.runningApp.bundleIdentifier == "SanGuoShaAirWD"
+    }
+}
+
+class SteamWindowCheck : WindowCheck {
+    override func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        // All Steam windows have subrole == AXUnknown
+        // some dropdown menus are not desirable; they have title == "", or sometimes role == nil when switching between menus quickly
+        return opts.runningApp.bundleIdentifier == "com.valvesoftware.steam" && opts.title != "" && opts.role != nil
+    }
+}
+
+class FirefoxFullscreenVideoWindowCheck : WindowCheck {
+    override func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        // Firefox fullscreen video have subrole == AXUnknown if fullscreen'ed when the base window is not fullscreen
+        return (opts.runningApp.bundleIdentifier?.hasPrefix("org.mozilla.firefox") ?? false) && opts.role == kAXWindowRole
+    }
+}
+
+class VLCFullscreenVideoWindowCheck : WindowCheck {
+    override func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        // VLC fullscreen video have subrole == AXUnknown if fullscreen'ed
+        return (opts.runningApp.bundleIdentifier?.hasPrefix("org.videolan.vlc") ?? false) && opts.role == kAXWindowRole
+    }
+}
+
+class AndroidEmulatorWindowCheck : WindowCheck {
+    override func isValidWindow(_ opts: WindowCheckOptions) -> Bool {
+        // android emulator small vertical menu is a "window" with empty title; we exclude it
+        return opts.title != "" && Applications.isAndroidEmulator(opts.runningApp)
+    }
+}
