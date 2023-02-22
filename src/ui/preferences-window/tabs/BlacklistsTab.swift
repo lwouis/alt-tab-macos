e74492c85688f95913ea18dc9161aaf9f59d8339
decodism <decodism@protonmail.com>
2023-02-22 16:30:22 +0100

    feat: allow batch removal from blacklist

--- src/ui/preferences-window/tabs/BlacklistsTab.swift
+++ src/ui/preferences-window/tabs/BlacklistsTab.swift
@@ -18,7 +18,7 @@ class BlacklistsTab {
                     }
                 }
             } else {
-                tableView.removeSelectedRow()
+                tableView.removeSelectedRows()
             }
         }
 
