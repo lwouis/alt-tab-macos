7d823dd61df7d615096ba9f2c80d3fb22e6afc96
Louis Pontoise <lwouis@gmail.com>
2023-06-14 23:21:01 +0200

    ci: replace altool with notarytool
    Apple will break altool notarization on Nov 2023, so we are forced to update our ci

--- scripts/package_and_notarize_release.sh
+++ scripts/package_and_notarize_release.sh
@@ -5,40 +5,21 @@ set -exu
 version="$(cat $VERSION_FILE)"
 appFile="$APP_NAME.app"
 zipName="$APP_NAME-$version.zip"
-bundleId="$(awk -F ' = ' '/PRODUCT_BUNDLE_IDENTIFIER/ { print $2; }' < config/base.xcconfig)"
+oldPwd="$PWD"
 
 cd "$XCODE_BUILD_PATH"
 ditto -c -k --keepParent "$appFile" "$zipName"
 
 # request notarization
-requestUuid=$(xcrun altool \
-  --notarize-app \
-  --primary-bundle-id "$bundleId" \
-  --username "$APPLE_ID" \
+requestStatus=$("$oldPwd"/scripts/notarytool submit \
+  --apple-id "$APPLE_ID" \
   --password "$APPLE_PASSWORD" \
-  --file "$zipName" 2>&1 |
+  --team-id "$APPLE_TEAM_ID" \
+  "$zipName" \
+  --wait --timeout 15m 2>&1 |
   tee /dev/tty |
-  awk '/RequestUUID/ { print $NF;exit; }')
-if [[ $requestUuid == "" ]]; then exit 1; fi
-
-# poll notarization status until success/invalid, or 15min have passed
-requestStatus="in progress"
-timeoutCounter=0
-until [[ "$requestStatus" == "success" ]] || [[ "$requestStatus" == "invalid" ]] || [[ $timeoutCounter -eq 1500 ]]; do
-  sleep 10
-  timeoutCounter=$((timeoutCounter+10))
-  set +e
-  requestLogs=$(xcrun altool \
-    --notarization-info "$requestUuid" \
-    --username "$APPLE_ID" \
-    --password "$APPLE_PASSWORD" 2>&1)
-  set -e
-  requestStatus=$(echo "$requestLogs" | awk -F ': ' '/Status:/ { print $2; }')
-done
-if [[ $requestStatus != "success" ]]; then
-  echo "$requestLogs" | awk -F ': ' '/LogFileURL:/ { print $2; }' | xargs curl
-  exit 1
-fi
+  awk -F ': ' '/  status:/ { print $2; }')
+if [[ $requestStatus != "Accepted" ]]; then exit 1; fi
 
 # staple build
 xcrun stapler staple "$appFile"
