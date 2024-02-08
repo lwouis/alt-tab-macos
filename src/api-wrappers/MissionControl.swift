bc29d1e86ce2b1512a3181faffe7887303f9a1d1
lwouis <lwouis@gmail.com>
2024-02-08 16:15:57 +0100

    refactor: apply compiler hints

--- src/api-wrappers/MissionControl.swift
+++ src/api-wrappers/MissionControl.swift
@@ -3,7 +3,6 @@ import Foundation
 class MissionControl {
     static func isActive() -> Bool {
         // when Mission Control is active, the Dock process spawns some windows. We observe this side-effect and infer
-        var missionControlHint = false
         for window in CGWindow.windows(.optionOnScreenOnly) {
             // ownerName == "Dock" && title == nil is a sign that Mission Control may be active
             if window.ownerName() == "Dock" && window.title() == nil
