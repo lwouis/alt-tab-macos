c43b56158a7f70ea126269e1917be65f2c265ec1
Ben Delaney <ben@bdelaney.me>
2024-06-09 16:44:35 +1000

    fix(scripts): update codesign identity variable
    Updated the codesign command in the codesign_sparkle_embedded_apps.sh script to use the $EXPANDED_CODE_SIGN_IDENTITY environment variable instead of $CODE_SIGN_IDENTITY, ensuring the correct identity is used for code signing the Sparkle embedded app AutoUpdate.
    
    This avoids "Apple Development: ambiguous" errors when building and running the app with automatic code signing enabled.

--- scripts/codesign_sparkle_embedded_apps.sh
+++ scripts/codesign_sparkle_embedded_apps.sh
@@ -8,4 +8,4 @@ set -exu
 # codesign --deep is only 1 level deep. It misses Sparkle embedded app AutoUpdate
 # this build phase script works around the issue
 
-codesign --verbose --force --sign "$CODE_SIGN_IDENTITY" $OTHER_CODE_SIGN_FLAGS "${CODESIGNING_FOLDER_PATH}/Contents/Frameworks/Sparkle.framework/Versions/A/Resources/Autoupdate.app"
+codesign --verbose --force --sign "$EXPANDED_CODE_SIGN_IDENTITY" $OTHER_CODE_SIGN_FLAGS "${CODESIGNING_FOLDER_PATH}/Contents/Frameworks/Sparkle.framework/Versions/A/Resources/Autoupdate.app"
