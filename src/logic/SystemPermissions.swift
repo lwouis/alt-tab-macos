f463ceb65be962860ca5e440a4ba39ac10fc670f
Dominic Vonk <dominic@vonk.io>
2024-07-19 09:55:54 +0200

    Format a bit

--- src/logic/SystemPermissions.swift
+++ src/logic/SystemPermissions.swift
@@ -2,108 +2,115 @@ import Cocoa
 
 // macOS has some privacy restrictions. The user needs to grant certain permissions, app by app, in System Preferences > Security & Privacy
 class SystemPermissions {
-    static var changeCallback: (() -> Void)!
-    static var timer: Timer!
+  static var changeCallback: (() -> Void)!
+  static var timer: Timer!
 
-    static func accessibilityIsGranted() -> Bool {
-        if #available(OSX 10.9, *) {
-            return AXIsProcessTrustedWithOptions([kAXTrustedCheckOptionPrompt.takeRetainedValue(): false] as CFDictionary)
-        }
-        return true
+  static func accessibilityIsGranted() -> Bool {
+    if #available(OSX 10.9, *) {
+      return AXIsProcessTrustedWithOptions(
+        [kAXTrustedCheckOptionPrompt.takeRetainedValue(): false] as CFDictionary)
     }
-    private static func canRecordScreen() -> Bool {
-        guard let windows = CGWindowListCopyWindowInfo([.optionOnScreenOnly], kCGNullWindowID) as? [[String: AnyObject]] else { return false }
-        return windows.allSatisfy({ window in
-            let windowName = window[kCGWindowName as String] as? String
-            return windowName != nil
-        })
+    return true
+  }
+  private static func canRecordScreen() -> Bool {
+    guard
+      let windows = CGWindowListCopyWindowInfo([.optionOnScreenOnly], kCGNullWindowID)
+        as? [[String: AnyObject]]
+    else { return false }
+    return windows.allSatisfy({ window in
+      let windowName = window[kCGWindowName as String] as? String
+      return windowName != nil
+    })
+  }
+  static func screenRecordingIsGranted() -> Bool {
+    if #available(macOS 15.0, *) {
+      return canRecordScreen()
     }
-    static func screenRecordingIsGranted() -> Bool {
-        if #available(macOS 15.0, *) {
-            return canRecordScreen()
-        }
-        if #available(OSX 10.15, *) {
-             return screenRecordingIsGranted_()
-        }
-        return true
+    if #available(OSX 10.15, *) {
+      return screenRecordingIsGranted_()
     }
+    return true
+  }
 
-    // workaround: public API CGPreflightScreenCaptureAccess and private API SLSRequestScreenCaptureAccess exist, but
-    // their return value is not updated during the app lifetime
-      // note: shows the system prompt if there's no permission
-      private static func screenRecordingIsGranted_() -> Bool {
-          return CGDisplayStream(
-              dispatchQueueDisplay: CGMainDisplayID(),
-              outputWidth: 1,
-              outputHeight: 1,
-              pixelFormat: Int32(kCVPixelFormatType_32BGRA),
-              properties: nil,
-              queue: .global(),
-              handler: { _, _, _, _ in }
-          ) != nil
-      }
+  // workaround: public API CGPreflightScreenCaptureAccess and private API SLSRequestScreenCaptureAccess exist, but
+  // their return value is not updated during the app lifetime
+  // note: shows the system prompt if there's no permission
+  private static func screenRecordingIsGranted_() -> Bool {
+    return CGDisplayStream(
+      dispatchQueueDisplay: CGMainDisplayID(),
+      outputWidth: 1,
+      outputHeight: 1,
+      pixelFormat: Int32(kCVPixelFormatType_32BGRA),
+      properties: nil,
+      queue: .global(),
+      handler: { _, _, _, _ in }
+    ) != nil
+  }
 
-    static func observePermissionsPostStartup() {
-        var counter = 0
-        timer = Timer(timeInterval: 5, repeats: true, block: { _ in
-            if !accessibilityIsGranted() {
-                App.app.restart()
-            }
-            if !screenRecordingIsGranted() {
-                // permission check may yield a false negative during wake-up
-                // we restart after 2 negative checks
-                if counter >= 2 {
-                    App.app.restart()
-                } else {
-                    counter += 1
-                }
-            } else {
-                counter = 0
-            }
-        })
-        timer.tolerance = 4.9
-        CFRunLoopAddTimer(BackgroundWork.systemPermissionsThread.runLoop, timer, .defaultMode)
-    }
-
-    static func observePermissionsPreStartup(_ startupBlock: @escaping () -> Void) {
-        if #available(macOS 15.0, *) {
-            CGRequestScreenCaptureAccess()
+  static func observePermissionsPostStartup() {
+    var counter = 0
+    timer = Timer(
+      timeInterval: 5, repeats: true,
+      block: { _ in
+        if !accessibilityIsGranted() {
+          App.app.restart()
         }
-        else if #available(OSX 10.15, *) {
-            SLSRequestScreenCaptureAccess()
-        }
-        timer = Timer(timeInterval: 0.1, repeats: true) { _ in
-            let accessibility = accessibilityIsGranted()
-            let screenRecording = screenRecordingIsGranted()
-            DispatchQueue.main.async {
-                if accessibility && screenRecording {
-                    timer.invalidate()
-                    App.app.permissionsWindow?.close()
-                    startupBlock()
-                } else {
-                    if accessibility != App.app.permissionsWindow.accessibilityView.isPermissionGranted {
-                        App.app.permissionsWindow.accessibilityView.updatePermissionStatus(accessibility)
-                    }
-                    if #available(OSX 10.15, *), screenRecording != App.app.permissionsWindow.screenRecordingView.isPermissionGranted {
-                        App.app.permissionsWindow.screenRecordingView.updatePermissionStatus(screenRecording)
-                    }
-                }
-            }
-        }
-        timer.tolerance = 0.09
-        CFRunLoopAddTimer(BackgroundWork.systemPermissionsThread.runLoop, timer, .defaultMode)
-    }
-
-    static func ensurePermissionsAreGranted(_ continueAppStartup: @escaping () -> Void) {
-        let startupBlock = {
-            observePermissionsPostStartup()
-            continueAppStartup()
-        }
-        if accessibilityIsGranted() && screenRecordingIsGranted() {
-            startupBlock()
+        if !screenRecordingIsGranted() {
+          // permission check may yield a false negative during wake-up
+          // we restart after 2 negative checks
+          if counter >= 2 {
+            App.app.restart()
+          } else {
+            counter += 1
+          }
         } else {
-            App.app.permissionsWindow.show()
-            observePermissionsPreStartup(startupBlock)
+          counter = 0
         }
+      })
+    timer.tolerance = 4.9
+    CFRunLoopAddTimer(BackgroundWork.systemPermissionsThread.runLoop, timer, .defaultMode)
+  }
+
+  static func observePermissionsPreStartup(_ startupBlock: @escaping () -> Void) {
+    if #available(macOS 15.0, *) {
+      CGRequestScreenCaptureAccess()
+    } else if #available(OSX 10.15, *) {
+      SLSRequestScreenCaptureAccess()
     }
+    timer = Timer(timeInterval: 0.1, repeats: true) { _ in
+      let accessibility = accessibilityIsGranted()
+      let screenRecording = screenRecordingIsGranted()
+      DispatchQueue.main.async {
+        if accessibility && screenRecording {
+          timer.invalidate()
+          App.app.permissionsWindow?.close()
+          startupBlock()
+        } else {
+          if accessibility != App.app.permissionsWindow.accessibilityView.isPermissionGranted {
+            App.app.permissionsWindow.accessibilityView.updatePermissionStatus(accessibility)
+          }
+          if #available(OSX 10.15, *),
+            screenRecording != App.app.permissionsWindow.screenRecordingView.isPermissionGranted
+          {
+            App.app.permissionsWindow.screenRecordingView.updatePermissionStatus(screenRecording)
+          }
+        }
+      }
+    }
+    timer.tolerance = 0.09
+    CFRunLoopAddTimer(BackgroundWork.systemPermissionsThread.runLoop, timer, .defaultMode)
+  }
+
+  static func ensurePermissionsAreGranted(_ continueAppStartup: @escaping () -> Void) {
+    let startupBlock = {
+      observePermissionsPostStartup()
+      continueAppStartup()
+    }
+    if accessibilityIsGranted() && screenRecordingIsGranted() {
+      startupBlock()
+    } else {
+      App.app.permissionsWindow.show()
+      observePermissionsPreStartup(startupBlock)
+    }
+  }
 }
