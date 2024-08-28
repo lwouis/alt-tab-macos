d215c960e723a6ce4de3c19accc3d69750530c1c
allsochen <erxiao.chen.gd@gmail.com>
2024-08-28 11:40:38 +0800

    fix: i8n

--- src/ui/preferences-window/tabs/appearance/AppearanceTab.swift
+++ src/ui/preferences-window/tabs/appearance/AppearanceTab.swift
@@ -240,7 +240,7 @@ class ShowHideIllustratedView {
         previewFocusedWindow.rightViews.append(LabelAndControl.makeInfoButton(width: 15, height: 15, onMouseEntered: { event, view in
             if ShowHideIllustratedView.isDisabledOnApplications(previewFocusedWindow) {
                 Popover.shared.show(event: event, positioningView: view,
-                        message: NSLocalizedString("Disabled in the switcher on applications mode. We can set the option at this window through `Advanced` -> `Show in switch`.", comment: ""))
+                        message: NSLocalizedString("Disabled in the switcher on applications mode. We can set the option at this window through 「Advanced」 -> 「Show in switch」.", comment: ""))
             } else {
                 Popover.shared.show(event: event, positioningView: view, message: previewFocusedWindow.subTitle!)
             }
