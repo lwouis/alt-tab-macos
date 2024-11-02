633ce3f0630cfe69f7b49c25f55484b2243ff0aa
lwouis <lwouis@gmail.com>
2024-11-02 16:54:47 +0100

    ci: switch to github actions (#1241)

--- scripts/ci-cd.sh
+++ scripts/ci-cd.sh
@@ -2,13 +2,18 @@
 
 set -exu
 
+pwd
 env | sort
 xcodebuild -version
 xcodebuild -workspace alt-tab-macos.xcworkspace -scheme Release -showBuildSettings | grep SWIFT_VERSION
 
 npm ci
-
-npx commitlint-travis
+#if travis
+#   npx commitlint-travis
+#   npx commitlint
+#else
+#  npx commitlint --from ${{ github.event.pull_request.base.sha }} --to ${{ github.event.pull_request.head.sha }} --verbose
+#fi
 scripts/ensure_generated_files_are_up_to_date.sh
 
 if [ $IS_RELEASE ]; then
@@ -20,9 +25,9 @@ scripts/codesign/setup_ci_master.sh
 xcodebuild -workspace alt-tab-macos.xcworkspace -scheme Release -derivedDataPath DerivedData
 file "$TRAVIS_BUILD_DIR/$XCODE_BUILD_PATH/$APP_NAME.app/Contents/MacOS/$APP_NAME"
 
-if [ $IS_RELEASE ]; then
-  scripts/package_and_notarize_release.sh
-  scripts/upload_symbols_to_appcenter.sh
-  scripts/update_appcast.sh
-  npx semantic-release
-fi
+#if [ $IS_RELEASE ]; then
+#  scripts/package_and_notarize_release.sh
+#  scripts/upload_symbols_to_appcenter.sh
+#  scripts/update_appcast.sh
+#  npx semantic-release
+#fi
