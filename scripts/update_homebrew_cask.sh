a966de9f503bb3d0ce3af93183b19564328b2886
lwouis <lwouis@gmail.com>
2024-11-02 23:11:25 +0100

    ci: replace cask-repair with bump-cask-pr

--- scripts/update_homebrew_cask.sh
+++ scripts/update_homebrew_cask.sh
@@ -2,16 +2,18 @@
 
 set -ex
 
-brew update
-brew install vitorgalvao/tiny-scripts/cask-repair
-
 version="$(cat $VERSION_FILE)"
 
-if [ "$TRAVIS" = true ] ; then
-  # add github ssh key
-  echo "$GITHUB_SSH_KEY" | base64 --decode > github_ssh
-  chmod 600 github_ssh
-  ssh-add -K github_ssh
-fi
+brew bump-cask-pr --version $version alt-tab
 
-cask-repair --blind-submit --cask-version "$version" alt-tab
+#brew update
+#brew install vitorgalvao/tiny-scripts/cask-repair
+#
+#if [ "$TRAVIS" = true ] ; then
+#  # add github ssh key
+#  echo "$GITHUB_SSH_KEY" | base64 --decode > github_ssh
+#  chmod 600 github_ssh
+#  ssh-add -K github_ssh
+#fi
+#
+#cask-repair --blind-submit --cask-version "$version" alt-tab
