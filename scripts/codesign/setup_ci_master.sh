8f23f7d74df26a44fc9490967ee0914ba721cccd
lwouis <lwouis@gmail.com>
2024-11-02 21:29:28 +0100

    ci: iterating on gh-actions

--- scripts/codesign/setup_ci_master.sh
+++ scripts/codesign/setup_ci_master.sh
@@ -7,4 +7,4 @@ certificateFile="codesign"
 # Recreate the certificate from the secure environment variable
 echo "$APPLE_P12_CERTIFICATE" | base64 --decode > $certificateFile.p12
 
-scripts/codesign/import_certificate_into_new_keychain.sh "$certificateFile" "$APPLE_P12_CERTIFICATE_PASSWORD"
+scripts/codesign/import_certificate_into_new_keychain.sh "$certificateFile" ""
