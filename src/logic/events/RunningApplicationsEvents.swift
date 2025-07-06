393915610d62fba987c304febb8c3cedc50f410b
Titouan V <titouan.verhille@gmail.com>
2025-07-06 18:51:08 +0200

    fix build on xcode 26

--- src/logic/events/RunningApplicationsEvents.swift
+++ src/logic/events/RunningApplicationsEvents.swift
@@ -2,15 +2,22 @@ import Cocoa
 
 class RunningApplicationsEvents {
     private static var appsObserver: NSKeyValueObservation!
-    private static var previousValueOfRunningApps: Set<NSRunningApplication>!
+    private static let appsQueue = DispatchQueue(label: "RunningApplicationsEvents.appsQueue")
+    private static var _previousValueOfRunningApps: Set<NSRunningApplication> = []
+    private static var previousValueOfRunningApps: Set<NSRunningApplication> {
+        get { appsQueue.sync { _previousValueOfRunningApps } }
+        set { appsQueue.sync { _previousValueOfRunningApps = newValue } }
+    }
 
     static func observe() {
         previousValueOfRunningApps = Set(NSWorkspace.shared.runningApplications)
-        appsObserver = NSWorkspace.shared.observe(\.runningApplications, options: [.old, .new], changeHandler: handleEvent)
+        appsObserver = NSWorkspace.shared.observe(\.runningApplications, options: [.old, .new], changeHandler: { workspace, change in
+            Self.handleEvent(workspace, change)
+        })
     }
 
     // TODO: handle this on a separate thread?
-    private static func handleEvent<A>(_: NSWorkspace, _ change: NSKeyValueObservedChange<A>) {
+    private static func handleEvent(_: NSWorkspace, _ change: NSKeyValueObservedChange<[NSRunningApplication]>) {
         let workspaceApps = Set(NSWorkspace.shared.runningApplications)
         // TODO: symmetricDifference has bad performance
         let diff = Array(workspaceApps.symmetricDifference(previousValueOfRunningApps))
