lwouis/alt-tab-macos

commit 3059dc2356a3db9dd379101daf8e4c0a8e874af0

perf: improve performance of the settings window

src/preferences/settings-window/tabs/controls/ShortcutsWhenActiveSheet.swift
@@ -1,32 +1,54 @@
 import Cocoa
 
 class ShortcutsWhenActiveSheet: SheetWindow {
+    private static let title = NSLocalizedString("Shortcuts When Active", comment: "")
+    private static let labelFocus = NSLocalizedString("Focus selected window", comment: "")
+    private static let labelPrevious = NSLocalizedString("Select previous window", comment: "")
+    private static let labelCancel = NSLocalizedString("Cancel", comment: "")
+    private static let labelSearch = NSLocalizedString("Search", comment: "")
+    private static let labelLockSearch = NSLocalizedString("Lock search", comment: "")
+    private static let labelClose = NSLocalizedString("Close window", comment: "")
+    private static let labelMinDemin = NSLocalizedString("Minimize/Deminimize window", comment: "")
+    private static let labelFullscreen = NSLocalizedString("Fullscreen/Defullscreen window", comment: "")
+    private static let labelQuit = NSLocalizedString("Quit app", comment: "")
+    private static let labelHideShow = NSLocalizedString("Hide/Show app", comment: "")
+
+    /// Pre-build search index for the open-button. See `SettingsSearchIndex.sheetSearchableStrings`.
+    /// `ProBadgeView.proLabel` contributes the "Pro" tag rendered on the search/lock-search rows.
+    static let searchableStrings: [String] = [
+        title,
+        labelFocus, labelPrevious, labelCancel,
+        labelSearch, labelLockSearch,
+        labelClose, labelMinDemin, labelFullscreen, labelQuit, labelHideShow,
+        ProBadgeView.proLabel,
+    ]
+
     override func makeContentView() -> NSView {
-        let focusWindowShortcut = TableGroupView.Row(leftTitle: NSLocalizedString("Focus selected window", comment: ""),
-            rightViews: [LabelAndControl.makeLabelWithRecorder(NSLocalizedString("Focus selected window", comment: ""), "focusWindowShortcut", Preferences.focusWindowShortcut, labelPosition: .right)[0]])
-        let previousWindowShortcut = TableGroupView.Row(leftTitle: NSLocalizedString("Select previous window", comment: ""),
-            rightViews: [LabelAndControl.makeLabelWithRecorder(NSLocalizedString("Select previous window", comment: ""), "previousWindowShortcut", Preferences.previousWindowShortcut, labelPosition: .right)[0]])
-        let cancelShortcut = TableGroupView.Row(leftTitle: NSLocalizedString("Cancel", comment: ""),
-            rightViews: [LabelAndControl.makeLabelWithRecorder(NSLocalizedString("Cancel", comment: ""), "cancelShortcut", Preferences.cancelShortcut, labelPosition: .right)[0]])
-        let closeWindowShortcut = TableGroupView.Row(leftTitle: NSLocalizedString("Close window", comment: ""),
-            rightViews: [LabelAndControl.makeLabelWithRecorder(NSLocalizedString("Close window", comment: ""), "closeWindowShortcut", Preferences.closeWindowShortcut, labelPosition: .right)[0]])
-        let minDeminWindowShortcut = TableGroupView.Row(leftTitle: NSLocalizedString("Minimize/Deminimize window", comment: ""),
-            rightViews: [LabelAndControl.makeLabelWithRecorder(NSLocalizedString("Minimize/Deminimize window", comment: ""), "minDeminWindowShortcut", Preferences.minDeminWindowShortcut, labelPosition: .right)[0]])
-        let toggleFullscreenWindowShortcut = TableGroupView.Row(leftTitle: NSLocalizedString("Fullscreen/Defullscreen window", comment: ""),
-            rightViews: [LabelAndControl.makeLabelWithRecorder(NSLocalizedString("Fullscreen/Defullscreen window", comment: ""), "toggleFullscreenWindowShortcut", Preferences.toggleFullscreenWindowShortcut, labelPosition: .right)[0]])
-        let quitAppShortcut = TableGroupView.Row(leftTitle: NSLocalizedString("Quit app", comment: ""),
-            rightViews: [LabelAndControl.makeLabelWithRecorder(NSLocalizedString("Quit app", comment: ""), "quitAppShortcut", Preferences.quitAppShortcut, labelPosition: .right)[0]])
-        let hideShowAppShortcut = TableGroupView.Row(leftTitle: NSLocalizedString("Hide/Show app", comment: ""),
-            rightViews: [LabelAndControl.makeLabelWithRecorder(NSLocalizedString("Hide/Show app", comment: ""), "hideShowAppShortcut", Preferences.hideShowAppShortcut, labelPosition: .right)[0]])
-        let table = TableGroupView(title: NSLocalizedString("Shortcuts When Active", comment: ""), width: SheetWindow.width)
+        let focusWindowShortcut = TableGroupView.Row(leftTitle: Self.labelFocus,
+            rightViews: [LabelAndControl.makeLabelWithRecorder(Self.labelFocus, "focusWindowShortcut", Preferences.focusWindowShortcut, labelPosition: .right)[0]])
+        let previousWindowShortcut = TableGroupView.Row(leftTitle: Self.labelPrevious,
+            rightViews: [LabelAndControl.makeLabelWithRecorder(Self.labelPrevious, "previousWindowShortcut", Preferences.previousWindowShortcut, labelPosition: .right)[0]])
+        let cancelShortcut = TableGroupView.Row(leftTitle: Self.labelCancel,
+            rightViews: [LabelAndControl.makeLabelWithRecorder(Self.labelCancel, "cancelShortcut", Preferences.cancelShortcut, labelPosition: .right)[0]])
+        let closeWindowShortcut = TableGroupView.Row(leftTitle: Self.labelClose,
+            rightViews: [LabelAndControl.makeLabelWithRecorder(Self.labelClose, "closeWindowShortcut", Preferences.closeWindowShortcut, labelPosition: .right)[0]])
+        let minDeminWindowShortcut = TableGroupView.Row(leftTitle: Self.labelMinDemin,
+            rightViews: [LabelAndControl.makeLabelWithRecorder(Self.labelMinDemin, "minDeminWindowShortcut", Preferences.minDeminWindowShortcut, labelPosition: .right)[0]])
+        let toggleFullscreenWindowShortcut = TableGroupView.Row(leftTitle: Self.labelFullscreen,
+            rightViews: [LabelAndControl.makeLabelWithRecorder(Self.labelFullscreen, "toggleFullscreenWindowShortcut", Preferences.toggleFullscreenWindowShortcut, labelPosition: .right)[0]])
+        let quitAppShortcut = TableGroupView.Row(leftTitle: Self.labelQuit,
+            rightViews: [LabelAndControl.makeLabelWithRecorder(Self.labelQuit, "quitAppShortcut", Preferences.quitAppShortcut, labelPosition: .right)[0]])
+        let hideShowAppShortcut = TableGroupView.Row(leftTitle: Self.labelHideShow,
+            rightViews: [LabelAndControl.makeLabelWithRecorder(Self.labelHideShow, "hideShowAppShortcut", Preferences.hideShowAppShortcut, labelPosition: .right)[0]])
+        let table = TableGroupView(title: Self.title, width: SheetWindow.width)
         _ = table.addRow(focusWindowShortcut)
         _ = table.addRow(previousWindowShortcut)
         _ = table.addRow(cancelShortcut)
-        let searchRow = table.addRow(leftText: NSLocalizedString("Search", comment: ""),
-            rightViews: [LabelAndControl.makeLabelWithRecorder(NSLocalizedString("Search", comment: ""), "searchShortcut", Preferences.searchShortcut, labelPosition: .right)[0]])
+        let searchRow = table.addRow(leftText: Self.labelSearch,
+            rightViews: [LabelAndControl.makeLabelWithRecorder(Self.labelSearch, "searchShortcut", Preferences.searchShortcut, labelPosition: .right)[0]])
         addProBadgeToLeftLabel(searchRow)
-        let lockSearchRow = table.addRow(leftText: NSLocalizedString("Lock search", comment: ""),
-            rightViews: [LabelAndControl.makeLabelWithRecorder(NSLocalizedString("Lock search", comment: ""), "lockSearchShortcut", Preferences.lockSearchShortcut, labelPosition: .right)[0]])
+        let lockSearchRow = table.addRow(leftText: Self.labelLockSearch,
+            rightViews: [LabelAndControl.makeLabelWithRecorder(Self.labelLockSearch, "lockSearchShortcut", Preferences.lockSearchShortcut, labelPosition: .right)[0]])
         addProBadgeToLeftLabel(lockSearchRow)
         _ = table.addRow(closeWindowShortcut)
         _ = table.addRow(minDeminWindowShortcut)

alt-tab-macos.xcodeproj/project.pbxproj
@@ -39,6 +39,7 @@
 		5E1EC110000000000000C002 /* SelectionResolver.swift in Sources */ = {isa = PBXBuildFile; fileRef = 5E1EC110000000000000C001 /* SelectionResolver.swift */; };
 		5E1EC110000000000000C003 /* SelectionResolver.swift in Sources */ = {isa = PBXBuildFile; fileRef = 5E1EC110000000000000C001 /* SelectionResolver.swift */; };
 		5E1EC110000000000000C102 /* SelectionResolverTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 5E1EC110000000000000C101 /* SelectionResolverTests.swift */; };
+		D04BAE2200000000ED004T11 /* SettingsSearchHighlightTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BAE2200000000ED004T12 /* SettingsSearchHighlightTests.swift */; };
 		5F04852E2F09AC9700235D4A /* WindowCaptureEvents.swift in Sources */ = {isa = PBXBuildFile; fileRef = 5F04852D2F09AC9700235D4A /* WindowCaptureEvents.swift */; };
 		5F0A63032F82D97C00AE4C38 /* LicenseManager.swift in Sources */ = {isa = PBXBuildFile; fileRef = 5F0A63012F82D97C00AE4C38 /* LicenseManager.swift */; };
 		5F0A63042F82D97C00AE4C38 /* LicenseState.swift in Sources */ = {isa = PBXBuildFile; fileRef = 5F0A63022F82D97C00AE4C38 /* LicenseState.swift */; };
@@ -106,6 +107,7 @@
 		5FDF9EB12FB3584300703843 /* Sparkle in Embed Frameworks */ = {isa = PBXBuildFile; productRef = 5FDF9EA52FB3575B00703843 /* Sparkle */; };
 		64C6A0192E08311D01D96E4E /* menubar-1.pdf in Resources */ = {isa = PBXBuildFile; fileRef = 126D5AA615CEE89F73C22DA7 /* menubar-1.pdf */; };
 		6D42BE46F595D567EDBF559A /* DynamicColorTextField.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BA0000000000000DCFFF1 /* DynamicColorTextField.swift */; };
+		D04BAE2100000000ED003001 /* LightLabel.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BAE2100000000ED003002 /* LightLabel.swift */; };
 		7D589F3A0C8DF93BA9E094C5 /* menubar-2.pdf in Resources */ = {isa = PBXBuildFile; fileRef = BC07818D6FCBEFC756C64296 /* menubar-2.pdf */; };
 		8B4A0B173EEB0648424219B5 /* ProBadgeView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 5F0A63072F82D97C00AE4C3A /* ProBadgeView.swift */; };
 		A053BAD6681629878E72B1F7 /* ProBadgeViewSegmentTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 25966EE691FC6871588D796B /* ProBadgeViewSegmentTests.swift */; };
@@ -184,6 +186,7 @@
 		BF0CBBBB0000000000001001 /* OnActionExtensionTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = BF0CBBBB0000000000000001 /* OnActionExtensionTests.swift */; };
 		C79742CA78D3CC74B6920437 /* menubar-0.pdf in Resources */ = {isa = PBXBuildFile; fileRef = 02540420CE27E6C973652426 /* menubar-0.pdf */; };
 		D04BA0000000000000DCFFF2 /* DynamicColorTextField.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BA0000000000000DCFFF1 /* DynamicColorTextField.swift */; };
+		D04BAE2100000000ED003003 /* LightLabel.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BAE2100000000ED003002 /* LightLabel.swift */; };
 		D04BA00000000000C0DE0002 /* SettingsSearch.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BA00000000000C0DE0001 /* SettingsSearch.swift */; };
 		D04BA00000000000C0DE0012 /* ShortcutAction.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BA00000000000C0DE0010 /* ShortcutAction.swift */; };
 		D04BA00000000000C0DE0021 /* SwitcherSession.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BA00000000000C0DE0020 /* SwitcherSession.swift */; };
@@ -243,6 +246,12 @@
 		D04BA9B7A964C5FABFEB9DA2 /* Localizable.strings in Resources */ = {isa = PBXBuildFile; fileRef = D04BA053A0E0157E817B2E04 /* Localizable.strings */; };
 		D04BA9D63A88AF2C20A1E656 /* SF-Pro-Text-Regular.otf in Resources */ = {isa = PBXBuildFile; fileRef = D04BA60A6D08C041F5BF8CC9 /* SF-Pro-Text-Regular.otf */; };
 		D04BAA16F21AC41A23DDA63D /* ControlsTab.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BA1B438BF3E65044D1138 /* ControlsTab.swift */; };
+		D04BAE1F00000000ED001001 /* ShortcutEditor.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BAE1F00000000ED001002 /* ShortcutEditor.swift */; };
+		D04BAE2000000000ED002001 /* SettingsSearchIndex.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BAE2000000000ED002002 /* SettingsSearchIndex.swift */; };
+		D04BAE2200000000ED004001 /* SettingsSearchHighlight.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BAE2200000000ED004002 /* SettingsSearchHighlight.swift */; };
+		D04BAE2200000000ED004003 /* SettingsSearchHighlight.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BAE2200000000ED004002 /* SettingsSearchHighlight.swift */; };
+		D04BAE2200000000ED004T01 /* TableGroupView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 1C961FB3DCF4906FEEF4A23D /* TableGroupView.swift */; };
+		D04BAE2200000000ED004T03 /* SettingsSearch.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BA00000000000C0DE0001 /* SettingsSearch.swift */; };
 		D04BAAAF5CFA991D3B078DB8 /* KeyboardEvents.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BA4A621A8A15660F973C1 /* KeyboardEvents.swift */; };
 		D04BAAB5C4E78AE375EA7D3C /* Localizable.strings in Resources */ = {isa = PBXBuildFile; fileRef = D04BAA56B4EEAF17F767C7FD /* Localizable.strings */; };
 		D04BAABE804F3769CE22BEB6 /* HyperlinkLabel.swift in Sources */ = {isa = PBXBuildFile; fileRef = D04BA49E45BFFF3D9FC60E43 /* HyperlinkLabel.swift */; };
@@ -341,6 +350,7 @@
 		5883F0A729A5182C0071DB65 /* TrackpadEvents.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = TrackpadEvents.swift; sourceTree = "<group>"; };
 		5E1EC110000000000000C001 /* SelectionResolver.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SelectionResolver.swift; sourceTree = "<group>"; };
 		5E1EC110000000000000C101 /* SelectionResolverTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SelectionResolverTests.swift; sourceTree = "<group>"; };
+		D04BAE2200000000ED004T12 /* SettingsSearchHighlightTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SettingsSearchHighlightTests.swift; sourceTree = "<group>"; };
 		5F04852D2F09AC9700235D4A /* WindowCaptureEvents.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = WindowCaptureEvents.swift; sourceTree = "<group>"; };
 		5F05D0E62F31441A004A95F9 /* AGENTS.md */ = {isa = PBXFileReference; lastKnownFileType = net.daringfireball.markdown; path = AGENTS.md; sourceTree = "<group>"; };
 		5F05D0E72F31441A004A95F9 /* CLAUDE.md */ = {isa = PBXFileReference; lastKnownFileType = net.daringfireball.markdown; path = CLAUDE.md; sourceTree = "<group>"; };
@@ -500,6 +510,7 @@
 		BF0CAAAA0000000000000001 /* SearchTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SearchTests.swift; sourceTree = "<group>"; };
 		BF0CBBBB0000000000000001 /* OnActionExtensionTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = OnActionExtensionTests.swift; sourceTree = "<group>"; };
 		D04BA0000000000000DCFFF1 /* DynamicColorTextField.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = DynamicColorTextField.swift; sourceTree = "<group>"; };
+		D04BAE2100000000ED003002 /* LightLabel.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = LightLabel.swift; sourceTree = "<group>"; };
 		D04BA00000000000C0DE0001 /* SettingsSearch.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SettingsSearch.swift; sourceTree = "<group>"; };
 		D04BA00000000000C0DE0010 /* ShortcutAction.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = ShortcutAction.swift; sourceTree = "<group>"; };
 		D04BA00000000000C0DE0020 /* SwitcherSession.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SwitcherSession.swift; sourceTree = "<group>"; };
@@ -518,6 +529,9 @@
 		D04BA1232AFEEFE90D5CC827 /* debug.xcconfig */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.xcconfig; path = debug.xcconfig; sourceTree = "<group>"; };
 		D04BA18B55AA6C9EE41985D0 /* contributing.md */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = net.daringfireball.markdown; path = contributing.md; sourceTree = "<group>"; };
 		D04BA1B438BF3E65044D1138 /* ControlsTab.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = ControlsTab.swift; sourceTree = "<group>"; };
+		D04BAE1F00000000ED001002 /* ShortcutEditor.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = ShortcutEditor.swift; sourceTree = "<group>"; };
+		D04BAE2000000000ED002002 /* SettingsSearchIndex.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SettingsSearchIndex.swift; sourceTree = "<group>"; };
+		D04BAE2200000000ED004002 /* SettingsSearchHighlight.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SettingsSearchHighlight.swift; sourceTree = "<group>"; };
 		D04BA1D80F4EEF2A91BAD29C /* release.config.js */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.javascript; path = release.config.js; sourceTree = "<group>"; };
 		D04BA1DF8CAB2FAB7FE9244B /* TileFontIconView.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = TileFontIconView.swift; sourceTree = "<group>"; };
 		D04BA2526DC6726E0F7ACF7C /* LabelAndControl.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = LabelAndControl.swift; sourceTree = "<group>"; };
@@ -787,6 +801,7 @@
 			isa = PBXGroup;
 			children = (
 				D04BA1B438BF3E65044D1138 /* ControlsTab.swift */,
+				D04BAE1F00000000ED001002 /* ShortcutEditor.swift */,
 				69A2E38AF41E866D3CDE71BF /* OverrideClickResolver.swift */,
 				5F21C9DF2C6E94240091F72F /* ShortcutsWhenActiveSheet.swift */,
 				5F21C9E12C6E94410091F72F /* AdditionalControlsSheet.swift */,
@@ -912,6 +927,7 @@
 				25966EE691FC6871588D796B /* ProBadgeViewSegmentTests.swift */,
 				4B4409E4B3D98C491503CE5D /* OverrideClickResolverTests.swift */,
 				5E1EC110000000000000C101 /* SelectionResolverTests.swift */,
+				D04BAE2200000000ED004T12 /* SettingsSearchHighlightTests.swift */,
 				BF0C829EAA93A4C4ADB42CF8 /* Mocks.swift */,
 				BF0C8C6810FF50908E637A48 /* ConcurrentScreenshots.swift */,
 				BF0C855CD9DBCAEFE48F9EA5 /* AXUIElementTests.swift */,
@@ -1425,6 +1441,7 @@
 			children = (
 				D04BACEE8D430B8CAAD8C4CD /* BoldLabel.swift */,
 				D04BA0000000000000DCFFF1 /* DynamicColorTextField.swift */,
+				D04BAE2100000000ED003002 /* LightLabel.swift */,
 				D04BA49E45BFFF3D9FC60E43 /* HyperlinkLabel.swift */,
 				D04BA7C836A8CE8C0B8D128B /* TextArea.swift */,
 				D04BA5BD11C365C19461458C /* TextField.swift */,
@@ -1467,6 +1484,8 @@
 				D04BA3FA52AA40B827A72F10 /* tabs */,
 				D04BA099FA155FB4D517D66B /* SettingsWindow.swift */,
 				D04BA00000000000C0DE0001 /* SettingsSearch.swift */,
+				D04BAE2000000000ED002002 /* SettingsSearchIndex.swift */,
+				D04BAE2200000000ED004002 /* SettingsSearchHighlight.swift */,
 				D04BAFF80A98CF287F85DA23 /* SidebarList.swift */,
 				1C961FB3DCF4906FEEF4A23D /* TableGroupView.swift */,
 			);
@@ -1757,8 +1776,13 @@
 				DC06402457ECCE6C902FEAE4 /* OverrideClickResolver.swift in Sources */,
 				5E1EC110000000000000C003 /* SelectionResolver.swift in Sources */,
 				5E1EC110000000000000C102 /* SelectionResolverTests.swift in Sources */,
+				D04BAE2200000000ED004T11 /* SettingsSearchHighlightTests.swift in Sources */,
+				D04BAE2200000000ED004003 /* SettingsSearchHighlight.swift in Sources */,
+				D04BAE2200000000ED004T01 /* TableGroupView.swift in Sources */,
+				D04BAE2200000000ED004T03 /* SettingsSearch.swift in Sources */,
 				8B4A0B173EEB0648424219B5 /* ProBadgeView.swift in Sources */,
 				6D42BE46F595D567EDBF559A /* DynamicColorTextField.swift in Sources */,
+				D04BAE2100000000ED003001 /* LightLabel.swift in Sources */,
 				BF0C8C5FA4B401A8C0F75B7A /* KeyboardEventsTestable.swift in Sources */,
 				F00DCAFE0000000000001103 /* SearchTestable.swift in Sources */,
 				BF0C875A28B98904F8F4A66A /* ATShortcut.swift in Sources */,
@@ -1828,6 +1852,7 @@
 				D04BA76A74267B1346D23687 /* GridView.swift in Sources */,
 				D04BAB4EB890853B5B9B2C61 /* BoldLabel.swift in Sources */,
 				D04BA0000000000000DCFFF2 /* DynamicColorTextField.swift in Sources */,
+				D04BAE2100000000ED003003 /* LightLabel.swift in Sources */,
 				D04BAABE804F3769CE22BEB6 /* HyperlinkLabel.swift in Sources */,
 				5F7B6AFC2F3BAB98003E25E8 /* StatusIconsView.swift in Sources */,
 				D04BAD2A7F2E8BF64EE982E9 /* TextArea.swift in Sources */,
@@ -1899,8 +1924,11 @@
 				D04BADA3F2DAB820D6FC6E75 /* AppCenterApplication.m in Sources */,
 				6E800CF06EE430DCCF9CE47B /* ObjCExceptionCatcher.m in Sources */,
 				D04BAA16F21AC41A23DDA63D /* ControlsTab.swift in Sources */,
+				D04BAE1F00000000ED001001 /* ShortcutEditor.swift in Sources */,
 				DD6771EE8ACE714C2599FEB7 /* OverrideClickResolver.swift in Sources */,
 				D04BADE4DDF7597749FB13BD /* SettingsWindow.swift in Sources */,
+				D04BAE2000000000ED002001 /* SettingsSearchIndex.swift in Sources */,
+				D04BAE2200000000ED004001 /* SettingsSearchHighlight.swift in Sources */,
 				BF0C8CC5057406014FD612CC /* ATShortcut.swift in Sources */,
 				D04BA00000000000C0DE0012 /* ShortcutAction.swift in Sources */,
 				D04BA00000000000C0DE0021 /* SwitcherSession.swift in Sources */,
@@ -2343,7 +2371,6 @@
 				GCC_WARN_UNUSED_FUNCTION = YES;
 				GCC_WARN_UNUSED_VARIABLE = YES;
 				GENERATE_INFOPLIST_FILE = YES;
-				MACOSX_DEPLOYMENT_TARGET = 13.1;
 				MARKETING_VERSION = 1.0;
 				MTL_ENABLE_DEBUG_INFO = NO;
 				MTL_FAST_MATH = YES;
@@ -2408,7 +2435,6 @@
 				GCC_WARN_UNUSED_FUNCTION = YES;
 				GCC_WARN_UNUSED_VARIABLE = YES;
 				GENERATE_INFOPLIST_FILE = YES;
-				MACOSX_DEPLOYMENT_TARGET = 13.1;
 				MARKETING_VERSION = 1.0;
 				MTL_ENABLE_DEBUG_INFO = INCLUDE_SOURCE;
 				MTL_FAST_MATH = YES;

config/test-base.xcconfig
@@ -2,6 +2,7 @@
 
 SWIFT_VERSION = 5.8
 SWIFT_TREAT_WARNINGS_AS_ERRORS = YES
+MACOSX_DEPLOYMENT_TARGET = 10.13
 CODE_SIGNING_REQUIRED = NO // no need for codesign
 CODE_SIGNING_ALLOWED = NO // no need for codesign
 ONLY_ACTIVE_ARCH = YES // only need to run on the host machine

src/kit/PopupButtonLikeSystemSettings.swift
@@ -1,34 +1,63 @@
 import Cocoa
 
+/// `NSPopUpButton` configured to look like System Settings popups and — critically — to size
+/// itself to the *currently-selected* item rather than the widest one (the AppKit default).
+///
+/// There is no public API to opt into "size to current item" — `NSPopUpButton` is hardcoded to
+/// reserve room for its widest item so the chrome doesn't visually resize when the user picks a
+/// different selection. To get pixel-perfect bezel + arrow + padding metrics, the only viable
+/// path is to override `intrinsicContentSize` and have AppKit measure a *probe* button that
+/// happens to contain just the current title.
+///
+/// The previous version did that probe measurement on *every* `intrinsicContentSize` query.
+/// AppKit queries the property many times per layout cycle, and the `_windowChangedKeyState`
+/// cascade fires a fresh layout cycle on every focus change — so we were running `sizeToFit`
+/// (which triggers TextKit2 layout on Tahoe) ~50 times per cascade × ~25 popups in Settings.
+/// Roughly 50ms of pure overhead per focus switch from this one method.
+///
+/// Fix: keep the probe technique (preserves pixel-identical sizing), cache the result keyed by
+/// the displayed title. The cache is invalidated automatically next time `title` changes (which
+/// happens on every selection mutation), so we don't have to override every individual mutator
+/// path. AppKit's repeated queries hit the cache in O(1).
 class PopupButtonLikeSystemSettings: NSPopUpButton {
+    private var cachedIntrinsicSize: NSSize?
+    private var cachedIntrinsicSizeTitle: String?
+
     convenience init() {
         self.init(frame: .zero)
         translatesAutoresizingMaskIntoConstraints = false
-//        showsBorderOnlyWhileMouseInside = true
-//        isBordered = false
-//        setButtonType(.switch)
-//        let cell = cell! as! NSPopUpButtonCell
-//        onAction = { _ in self.sizeToFit() }
-//        cell.bezelStyle = .regularSquare
-//        cell.arrowPosition = .arrowAtBottom
-//        cell.imagePosition = .imageOverlaps
     }
 
     override var intrinsicContentSize: NSSize {
-        if let selectedItem {
-            let fakePopUpButton = NSPopUpButton()
-            fakePopUpButton.addItem(withTitle: title)
-            fakePopUpButton.item(at: 0)!.image = selectedItem.image
-            let fakeCell = fakePopUpButton.cell! as! NSPopUpButtonCell
-            let currentCell = cell! as! NSPopUpButtonCell
-            fakeCell.bezelStyle = currentCell.bezelStyle
-            fakeCell.arrowPosition = currentCell.arrowPosition
-            fakeCell.imagePosition = currentCell.imagePosition
-            fakePopUpButton.showsBorderOnlyWhileMouseInside = showsBorderOnlyWhileMouseInside
-            fakePopUpButton.sizeToFit()
-            return fakePopUpButton.intrinsicContentSize
-        } else {
-            return super.intrinsicContentSize
+        let currentTitle = title
+        if let cached = cachedIntrinsicSize, cachedIntrinsicSizeTitle == currentTitle {
+            return cached
         }
+        let size: NSSize
+        if let selectedItem {
+            size = measureSize(for: currentTitle, image: selectedItem.image)
+        } else {
+            size = super.intrinsicContentSize
+        }
+        cachedIntrinsicSize = size
+        cachedIntrinsicSizeTitle = currentTitle
+        return size
+    }
+
+    /// Probe measurement — pixel-identical to what AppKit would compute for a popup whose menu
+    /// has a single item with this title. Heavy (full TextKit layout via `sizeToFit`), hence
+    /// the caller caches the result.
+    private func measureSize(for titleText: String, image: NSImage?) -> NSSize {
+        let probe = NSPopUpButton()
+        probe.addItem(withTitle: titleText)
+        probe.item(at: 0)!.image = image
+        let probeCell = probe.cell! as! NSPopUpButtonCell
+        let selfCell = cell! as! NSPopUpButtonCell
+        probeCell.bezelStyle = selfCell.bezelStyle
+        probeCell.arrowPosition = selfCell.arrowPosition
+        probeCell.imagePosition = selfCell.imagePosition
+        probe.showsBorderOnlyWhileMouseInside = showsBorderOnlyWhileMouseInside
+        probe.sizeToFit()
+        return probe.intrinsicContentSize
     }
 }

commit 736cf86049fffd3866df6178a70f7e24efc1ed47

fix: show the Pro badge on the Search row again
It broke in 3059dc23 when TableGroupView.makeText switched the row title from
NSTextField to a LightLabel (an NSView): addProBadgeToLeftLabel cast the label with
`as? NSTextField`, which then silently failed, so the badge was never added. Unwrap the
label as its NSView base instead (the badge only needs view anchors), so a future
makeText return-type change can't re-break it.

src/preferences/settings-window/tabs/controls/ShortcutsWhenActiveSheet.swift
@@ -57,7 +57,11 @@ class ShortcutsWhenActiveSheet: SheetWindow {
     }
 
     private func addProBadgeToLeftLabel(_ rowInfo: TableGroupView.RowInfo) {
-        guard let label = rowInfo.leftViews?.first as? NSTextField else { return }
+        // Anchor the badge to the row's left label. Don't cast to a concrete view type: the label used
+        // to be an `NSTextField` but `TableGroupView.makeText` now returns a `LightLabel` (an `NSView`),
+        // and the old `as? NSTextField` silently failed that cast, dropping the badge. We only need
+        // NSView anchors, so unwrap as-is and stay agnostic to future label type changes.
+        guard let label = rowInfo.leftViews?.first else { return }
         let badge = ProBadgeView()
         label.superview?.addSubview(badge)
         NSLayoutConstraint.activate([